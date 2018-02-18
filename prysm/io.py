c56801b5d5dcbe2e943f9e67c70165ed02ab3d3d
Brandon Dube <brandondube@gmail.com>
2018-02-17 20:45:08 -0500

    the great color science exodus of 2018
    migrated to new raynbow repository

--- prysm/io.py
+++ prysm/io.py
@@ -9,52 +9,6 @@ from prysm.conf import config
 from prysm.mtf_utils import MTFvFvF
 
 
-def read_oceanoptics(file_path):
-    """Read spectral transmission data from an ocean optics spectrometer.
-
-    Parameters
-    ----------
-    file_path : `str` or path_like
-        path to a file
-
-    Returns
-    -------
-    `dict`
-        a dictionary with keys of wvl and values
-
-    Raises
-    ------
-    `IOError`
-        if the file is malformed.
-
-    """
-    with open(file_path, 'r') as fid:
-        txtlines = fid.readlines()
-
-        idx, ready_length, ready_spectral = None, False, False
-        for i, line in enumerate(txtlines):
-            if 'Number of Pixels in Spectrum' in line:
-                length, ready_length = int(line.split()[-1]), True
-            elif '>>>>>Begin Spectral Data<<<<<' in line:
-                idx, ready_spectral = i, True
-
-        if not ready_length or not ready_spectral:
-            raise IOError('''File lacks line stating "Number of Pixels in Spectrum" or
-                             ">>>>>Begin Spectral Data<<<<<" and appears to be corrupt.''')
-        data_lines = txtlines[idx + 1:]
-        wavelengths = np.empty(length, dtype=config.precision)
-        values = np.empty(length, dtype=config.precision)
-        for idx, line in enumerate(data_lines):
-            wvl, val = line.split()
-            wavelengths[idx] = wvl
-            values[idx] = val
-
-        return {
-            'wvl': wavelengths,
-            'values': values,
-        }
-
-
 def read_trioptics_MTFvFvF(file_path):
     """Read MTF vs Field vs Focus data from a Trioptics .txt dump.
 
