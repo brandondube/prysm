f6115b22cbd18eabd86c4911037a00f58bf61909
Brandon Dube <brandondube@gmail.com>
2018-03-07 11:29:20 -0500

    comments for clarity

--- prysm/convolution.py
+++ prysm/convolution.py
@@ -172,6 +172,7 @@ def pure_numerical_ft_convolution(convolvable1, convolvable2):
         A convolution result
 
     """
+    # logic tree of convolvable cases with specific implementations
     if (convolvable1.sample_spacing - convolvable2.sample_spacing) < 1e-4:
         s1, s2 = convolvable1.data.shape, convolvable2.data.shape
         if s1[0] > s2[0]:
@@ -185,6 +186,7 @@ def pure_numerical_ft_convolution(convolvable1, convolvable2):
 
 
 def _numerical_ft_convolution_core_equalspacing(convolvable1, convolvable2):
+    # two are identically sampled; just FFT convolve them without modification
     ft1 = fftshift(fft2(fftshift(convolvable1.data)))
     ft2 = fftshift(fft2(fftshift(convolvable2.data)))
     data = abs(fftshift(ifft2(ft1 * ft2)))
@@ -192,26 +194,31 @@ def _numerical_ft_convolution_core_equalspacing(convolvable1, convolvable2):
 
 
 def _numerical_ft_convolution_core_equalspacing_unequalsamplecount(more_samples, less_samples):
+    # compute the ordinate axes of the input and output
+    in_x = forward_ft_unit(less_samples.sample_spacing, less_samples.data.shape[0])
+    in_y = forward_ft_unit(less_samples.sample_spacing, less_samples.data.shape[1])
     output_x = forward_ft_unit(more_samples.sample_spacing, more_samples.data.shape[0])
     output_y = forward_ft_unit(more_samples.sample_spacing, more_samples.data.shape[1])
 
-    in_x = forward_ft_unit(less_samples.sample_spacing, less_samples.data.shape[0])
-    in_y = forward_ft_unit(less_samples.sample_spacing, less_samples.data.shape[1])
+    # FFT the less sampled one and map it onto the denser grid
     less_fourier = fftshift(fft2(fftshift(less_samples.data)))
     interpf = interp2d(in_x, in_y, less_fourier, kind='linear')
-
     resampled_less = interpf(output_x, output_y)
+
+    # FFT convolve the two convolvables
     more_fourier = fftshift(fft2(fftshift(more_samples.data)))
     data = abs(fftshift(ifft2(resampled_less * more_fourier)))
     return Convolvable(data, more_samples.unit_x, more_samples.unit_y, False)
 
 
 def _compute_output_grid(convolvable1, convolvable2):
+    # determine output spacing
     if convolvable1.sample_spacing < convolvable2.sample_spacing:
         output_spacing = convolvable1.sample_spacing
     else:
         output_spacing = convolvable2.sample_spacing
 
+    # determine region of output
     if convolvable1.unit_x[0] < convolvable2.unit_x[0]:
         output_x_left = convolvable1.unit_x[0]
     else:
@@ -222,6 +229,7 @@ def _compute_output_grid(convolvable1, convolvable2):
     else:
         output_y_right = convolvable2.unit_y[0]
 
+    # if region is not an integer multiple of sample spacings, enlarge to make this true
     x_rem = (output_x_right - output_x_left) % output_spacing
     y_rem = (output_y_right - output_y_left) % output_spacing
     if x_rem > 1e-3:
@@ -233,6 +241,7 @@ def _compute_output_grid(convolvable1, convolvable2):
         output_y_left -= adj
         output_y_right += adj
 
+    # finally, compute the output window
     samples_x = (output_x_right - output_x_left) // output_spacing
     samples_y = (output_y_right - output_y_left) // output_spacing
     unit_out_x = np.linspace(output_x_left, output_x_right, samples_x)
