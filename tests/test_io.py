06e4c81322a8e0b8a2f667d277c9adf5cb1f4cc2
Brandon Dube <brandondube@gmail.com>
2018-04-02 22:41:59 -0400

    update test associated with read MTFvFvF

--- tests/test_io.py
+++ tests/test_io.py
@@ -11,7 +11,7 @@ data_root = Path(__file__).parent / 'io_files'
 
 def test_read_mtfvfvf_functions():
     p = data_root / 'valid_sample_MTFvFvF_Sag.txt'
-    result = io.read_trioptics_MTFvFvF(p)
+    result = io.read_trioptics_mtfvfvf(p)
     assert result
 
 
