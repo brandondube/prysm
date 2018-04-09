862c58b5933b1a5de0b1beb8df62b832ec2d5d32
Brandon Dube <brandondube@gmail.com>
2018-04-09 01:52:38 -0400

    breaking changes in PixelAperture and OLPF => version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='prysm',
-    version='0.9.1',
+    version='0.10.0',
     description='A python optics module',
     long_description='Uses geometrical and fourier optics to model optical systems',
     license='MIT',
