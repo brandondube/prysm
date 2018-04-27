93e7ff94ac6895b8d7dbd6aa02c04ca825e15e06
Brandon Dube <brandondube@gmail.com>
2018-04-27 15:32:51 -0400

    new convolvable plotting tests

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -1,3 +1,6 @@
+
+import numpy as np
+
 import pytest
 
 from prysm import PixelAperture, Pupil, PSF
@@ -39,3 +42,27 @@ def test_numerical_convolution_equal_functions(sample_psf):
 
 def test_numerical_convolution_unequal_functions(sample_psf, sample_psf_bigger):
     assert sample_psf.conv(sample_psf_bigger)
+
+
+def test_show_functions(sample_psf):
+    fig, ax = sample_psf.show(show_colorbar=True)
+    assert fig
+    assert ax
+
+
+def test_show_fourier_functions_numerical(sample_psf):
+    fig, ax = sample_psf.show_fourier()
+    assert fig
+    assert ax
+
+
+def test_show_fourier_functions_analytic(sample_pixel):
+    x = y = np.linspace(-10, 10, 10)
+    fig, ax = sample_pixel.show_fourier(freq_x=x, freq_y=y)
+    assert fig
+    assert ax
+
+
+def test_show_fourier_raises_analytic_with_no_xy(sample_pixel):
+    with pytest.raises(ValueError):
+        sample_pixel.show_fourier()
