4a19b05dd925a3482e60fa05232d2c4ed868aeb8
Brandon Dube <brandondube@gmail.com>
2018-05-26 20:45:01 -0400

    + metadata scanner for trioptics mht files

--- prysm/io.py
+++ prysm/io.py
@@ -2,11 +2,24 @@
 import os
 import re
 import codecs
+import datetime
+import calendar
 
 from .conf import config
 from prysm import mathops as m
 
 trioptics_enc = 'cp1252'
+
+
+def read_file_stream_or_path(path_or_file):
+    try:
+        with codecs.open(path_or_file, mode='r', encoding=trioptics_enc) as fid:
+            data = codecs.encode(fid.read(), 'utf-8').decode('utf-8')
+
+    except FileNotFoundError:
+        data = path_or_file.read()
+
+    return data
 
 
 def read_trioptics_mtfvfvf(file_path):
@@ -69,37 +82,35 @@ def read_trioptics_mtf_vs_field(file_path):
         dictionary with keys of freq, field, tan, sag
 
     """
-    # read the file into memory and handle ISO-8859-1 to UTF-8 for non windows platforms
-    with codecs.open(file_path, mode='r', encoding=trioptics_enc) as fid:
-        data = codecs.encode(fid.read(), 'utf-8').decode('utf-8')
+    data = read_file_stream_or_path(file_path)
 
-        # compile a pattern that will search for the image heights in the file and extract
-        fields_pattern = re.compile(f'MTF=09{os.linesep}(.*?){os.linesep}Legend=09', flags=re.DOTALL)
-        fields = fields_pattern.findall(data)[0]  # two copies, only need 1st
+    # compile a pattern that will search for the image heights in the file and extract
+    fields_pattern = re.compile(f'MTF=09{os.linesep}(.*?){os.linesep}Legend=09', flags=re.DOTALL)
+    fields = fields_pattern.findall(data)[0]  # two copies, only need 1st
 
-        # make a pattern that will search for and extract the tan and sag MTF data.  The match will
-        # return two copies; one for vs imght, one for vs angle.  Only keep half the matches.
-        tan_pattern = re.compile(r'Tan(.*?)=97', flags=re.DOTALL)
-        sag_pattern = re.compile(r'Sag(.*?)=97', flags=re.DOTALL)
-        tan, sag = tan_pattern.findall(data), sag_pattern.findall(data)
-        endpt = len(tan) // 2
-        tan, sag = tan[:endpt], sag[:endpt]
+    # make a pattern that will search for and extract the tan and sag MTF data.  The match will
+    # return two copies; one for vs imght, one for vs angle.  Only keep half the matches.
+    tan_pattern = re.compile(r'Tan(.*?)=97', flags=re.DOTALL)
+    sag_pattern = re.compile(r'Sag(.*?)=97', flags=re.DOTALL)
+    tan, sag = tan_pattern.findall(data), sag_pattern.findall(data)
+    endpt = len(tan) // 2
+    tan, sag = tan[:endpt], sag[:endpt]
 
-        # now extract the freqs from the tan data
-        freqs = m.asarray([float(s.split('(')[0][1:]) for s in tan])
+    # now extract the freqs from the tan data
+    freqs = m.asarray([float(s.split('(')[0][1:]) for s in tan])
 
-        # lastly, extract the floating point tan and sag data
-        # also take fields, to the 4th decimal place (nearest .1um)
-        # reformat T/S to 2D arrays with indices of (freq, field)
-        tan = m.asarray([s.split('=09')[1:-1] for s in tan], dtype=config.precision)
-        sag = m.asarray([s.split('=09')[1:-1] for s in sag], dtype=config.precision)
-        fields = m.asarray(fields.split('=09')[0:-1], dtype=config.precision).round(4)
-        return {
-            'freq': freqs,
-            'field': fields,
-            'tan': tan,
-            'sag': sag,
-        }
+    # lastly, extract the floating point tan and sag data
+    # also take fields, to the 4th decimal place (nearest .1um)
+    # reformat T/S to 2D arrays with indices of (freq, field)
+    tan = m.asarray([s.split('=09')[1:-1] for s in tan], dtype=config.precision)
+    sag = m.asarray([s.split('=09')[1:-1] for s in sag], dtype=config.precision)
+    fields = m.asarray(fields.split('=09')[0:-1], dtype=config.precision).round(4)
+    return {
+        'freq': freqs,
+        'field': fields,
+        'tan': tan,
+        'sag': sag,
+    }
 
 
 def read_trioptics_mtf(file_path):
@@ -116,15 +127,12 @@ def read_trioptics_mtf(file_path):
         dictionary with keys focus, wavelength, freq, tan, sag
 
     """
-    # read the file into memory and handle ISO-8859-1 to UTF-8 for non windows platforms
-    with codecs.open(file_path, mode='r', encoding=trioptics_enc) as fid:
-        data = codecs.encode(fid.read(), 'utf-8').decode('utf-8')
+    data = read_file_stream_or_path(file_path)
 
     # compile regex scanners to grab wavelength, focus, and frequency information
     # in addition to the T, S MTF data.
     # lastly, compile a scanner to cut the file after the end of the "MTF Sagittal" scanner
     focus_scanner = re.compile(r'Focus Position  : (\-?\d+\.\d+) mm')
-    wavelength_scanner = re.compile(r'Wavelength      : (\d+) nm')
     data_scanner = re.compile(r'\r\n(\d+\.?\d?)=09\r\n(\d+\.\d+)=09')
     sag_scanner = re.compile(r'Measurement Table: MTF vs. Frequency \( Sagittal \)')
     blockend_scanner = re.compile(r'  _____ =20')
@@ -136,7 +144,6 @@ def read_trioptics_mtf(file_path):
 
     # get focus and wavelength
     focus_pos = float(focus_scanner.search(data).group(1))
-    wavelength = float(wavelength_scanner.search(data).group(1)) / 1e3  # nm to um
 
     # simultaneously grab frequency and MTF
     result = data_scanner.findall(data[:cutoff])
@@ -152,8 +159,51 @@ def read_trioptics_mtf(file_path):
 
     return {
         'focus': focus_pos,
-        'wavelength': wavelength,
         'freq': freqs,
         'tan': t,
         'sag': s,
+    }
+
+
+def parse_trioptics_metadata(file_contents):
+    data = file_contents[750:1500]  # skip large section to make regex faster
+
+    operator_scanner = re.compile(r'Operator         : (\S*)')
+    time_scanner = re.compile(r'Time/Date        : (\d{2}:\d{2}:\d{2}\s*\w*\s*\d*,\s*\d*)')
+    sampleid_scanner = re.compile(r'Sample ID        : (.*)')
+    instrument_sn_scanner = re.compile(r'Instrument S/N   : (\S*)')
+
+    collimatorefl_scanner = re.compile(r'EFL \(Collimator\): (\d*) mm')
+    wavelength_scanner = re.compile(r'Wavelength      : (\d+) nm')
+    sampleefl_scanner = re.compile(r'EFL \(Sample\)    : (\d*\.\d*) mm')
+    objangle_scanner = re.compile(r'Object Angle    : (-?\d*\.\d*) =B0')
+    focuspos_scanner = re.compile(r'Focus Position  : (\d*\.\d*) mm')
+    azimuth_scanner = re.compile(r'Sample Azimuth  : (-?\d*\.\d*) =B0')
+
+    operator = operator_scanner.search(data).group(1)
+    time = time_scanner.search(data).group(1)
+    hms, month, day, year = time.split()
+    month_num = list(calendar.month_name).index(month)
+    h, m, s = hms.split(':')
+    timestamp = datetime.datetime(year=year, month=month_num, day=day, hour=h, minute=m, second=s)
+    sampleid = sampleid_scanner.search(data).group(1)
+    instrument_sn = instrument_sn_scanner.search(data).group(1)
+
+    collimator_efl = float(collimatorefl_scanner.search(data).group(1))
+    wavelength = float(wavelength_scanner.search(data).group(1)) / 1e3  # nm to um
+    sample_efl = float(sampleefl_scanner.search(data).group(1))
+    obj_angle = float(objangle_scanner.search(data).group(1))
+    focus_pos = float(focuspos_scanner.search(data).group(1))
+    azimuth = float(azimuth_scanner.search(data).group(1))
+    return {
+        'operator': operator,
+        'time': timestamp,
+        'sample_id': sampleid,
+        'instrument_sn': instrument_sn,
+        'collimator': collimator_efl,
+        'wavelength': wavelength,
+        'efl': sample_efl,
+        'obj_angle': obj_angle,
+        'focus_pos': focus_pos,
+        'azimuth': azimuth,
     }
