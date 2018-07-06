3f5f1bd66152164758cc7d23683bab8c27790014
Brandon Dube <brandondube@gmail.com>
2018-07-06 11:36:54 -0400

    add bandreject filter

--- prysm/interferogram.py
+++ prysm/interferogram.py
@@ -1,6 +1,7 @@
 """tools to analyze interferometric data."""
 from prysm.io import read_zygo_dat
 from prysm.util import share_fig_ax, pv, rms, Ra
+from prysm.fttools import forward_ft_unit
 from prysm import mathops as m
 
 
@@ -18,9 +19,11 @@ class Interferogram(object):
             self.x = m.arange(phase.shape[1])
             self.y = m.arange(phase.shape[0])
             self._realxy = False
+            self.lateral_res = None
         else:
             self.x, self.y = x, y
             self._realxy = True
+            self.sample_spacing = x[1] - x[0]
 
     @property
     def pv(self):
@@ -51,6 +54,12 @@ class Interferogram(object):
         self.remove_piston()
         return self
 
+    def bandreject(self, wllow, wlhigh):
+        new_phase = bandreject_filter(self.phase, self.sample_spacing, wllow, wlhigh)
+        new_phase[~m.isfinite(self.phase)] = m.nan
+        self.phase = new_phase
+        return self
+
     def plot2d(self, cmap='inferno', interp_method='lanczos', fig=None, ax=None):
         """Plot the data in 2D.
 
@@ -79,7 +88,7 @@ class Interferogram(object):
                        extent=[self.x[0], self.x[-1], self.y[0], self.y[-1]],
                        cmap=cmap,
                        interpolation=interp_method)
-        cb = fig.colorbar(im, label='Height [nm]', ax=ax, fraction=0.046)
+        fig.colorbar(im, label='Height [nm]', ax=ax, fraction=0.046)
         xlab, ylab = self.__class__.labels[self._realxy]
         ax.set(xlabel=xlab, ylabel=ylab)
         return fig, ax
@@ -103,3 +112,27 @@ def fit_plane(x, y, z):
     coefs = m.lstsq(m.stack([xx_, yy_, flat]).T, z[pts].flatten(), rcond=None)[0]
     plane_fit = coefs[0] * xx + coefs[1] * yy + coefs[2]
     return plane_fit
+
+
+def bandreject_filter(array, sample_spacing, wllow, wlhigh):
+    sy, sx = array.shape
+
+    # compute the bandpass in sample coordinates
+    ux, uy = forward_ft_unit(sample_spacing, sx), forward_ft_unit(sample_spacing, sy)
+    fhigh, flow = 1/wllow, 1/wlhigh
+
+    # make an ordinate array in frequency space and use it to make a mask
+    uxx, uyy = m.meshgrid(ux, uy)
+    highpass = ((uxx < -fhigh) | (uxx > fhigh)) | ((uyy < -fhigh) | (uyy > fhigh))
+    lowpass = ((uxx > -flow) & (uxx < flow)) & ((uyy > -flow) & (uyy < flow))
+    mask = highpass | lowpass
+
+    # adjust NaNs and FFT
+    work = array.copy()
+    work[~m.isfinite(work)] = 0
+    fourier = m.fftshift(m.fft2(m.ifftshift(work)))
+    fourier[mask] = 0
+    out = m.fftshift(m.ifft2(m.ifftshift(fourier)))
+    return out.real
+
+
