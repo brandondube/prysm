brandondube/prysm

commit ac508863f06dc206c80fb4ca0e1412a1a9428209

broken refactor of pupil and interferograms into optical phases

prysm/pupil.py
@@ -2,15 +2,15 @@
 '''
 from copy import deepcopy
 
+from ._phase import OpticalPhase
 from .conf import config
-from .util import share_fig_ax, pv, rms
 from .coordinates import make_rho_phi_grid
 from .geometry import mcache
 
 from prysm import mathops as m
 
 
-class Pupil(object):
+class Pupil(OpticalPhase):
     """Pupil of an optical system.
 
     Properties
@@ -19,10 +19,9 @@ class Pupil(object):
              data where x is the sample coordinate and y is the phase.
     slice_y: slice through the y axis of the pupil.  Returns (x,y)
              data where x is the sample coordinate and y is the phase.
-
     pv: Peak-To-Valley wavefront error.
-
     rms: Root Mean Square wavefront error.
+    Ra: Ra wavefront error.
 
     Notes
     -----
@@ -57,7 +56,7 @@ class Pupil(object):
         target for automatic masking on pupil creation
 
     """
-    def __init__(self, samples=128, epd=1.0, wavelength=0.55, opd_unit=r'$\lambda$',
+    def __init__(self, samples=128, epd=1.0, wavelength=0.55, opd_unit=r'waves',
                  mask='circle', mask_target='both'):
         """Create a new `Pupil` instance.
 
@@ -89,27 +88,17 @@ class Pupil(object):
             if the OPD unit given is invalid
 
         """
-        self.samples = samples
         self.epd = epd
-        self.wavelength = wavelength
-        self.opd_unit = opd_unit
-        self.phase = self.fcn = m.empty((samples, samples), dtype=config.precision)
-        self.unit = m.linspace(-epd / 2, epd / 2, samples, dtype=config.precision)
-        self.sample_spacing = self.unit[-1] - self.unit[-2]
-        self.rho = self.phi = m.empty((samples, samples), dtype=config.precision)
-        self.center = samples // 2
-
-        if opd_unit.lower() in ('$\lambda$', 'waves'):
-            self._opd_unit = 'waves'
-            self._opd_str = '$\lambda$'
-        elif opd_unit.lower() in ('$\mu m$', 'microns', 'micrometers', 'um'):
-            self._opd_unit = 'microns'
-            self._opd_str = '$\mu m$'
-        elif opd_unit.lower() in ('nm', 'nanometers'):
-            self._opd_unit = 'nanometers'
-            self._opd_str = 'nm'
-        else:
-            raise ValueError('OPD must be m.expressed in waves, microns, or nm')
+        ux = m.linspace(-epd//2, epd//2, samples)
+        uy = m.linspace(-epd//2, epd//2, samples)
+        self.samples = samples
+        self.sample_spacing = ux[1] - ux[0]
+        super().__init__(unit_x=ux, unit_y=uy, phase=None,
+                         wavelength=wavelength, phase_unit=opd_unit, spatial_unit='mm')
+        self.xaxis_label = 'Pupil ξ'
+        self.yaxis_label = 'Pupil η'
+        self.zaxis_label = 'OPD'
+        self.rho = self.phi = None
 
         if type(mask) is not m.ndarray:
             mask = mcache.get_mask(self.samples, mask)
@@ -119,155 +108,6 @@ class Pupil(object):
 
         self.build()
         self.mask(self._mask, self.mask_target)
-
-    # quick-access slices, properties ------------------------------------------
-
-    @property
-    def slice_x(self):
-        """Retrieve a slice through the X axis of the `Pupil`.
-
-        Returns
-        -------
-        self.unit : `numpy.ndarray`
-            ordinate axis
-        slice of self.phase : `numpy.ndarray`
-
-        """
-        return self.unit, self.phase[self.center]
-
-    @property
-    def slice_y(self):
-        """Retrieve a slice through the Y axis of the `Pupil`.
-
-        Returns
-        -------
-        self.unit : `numpy.ndarray`
-            ordinate axis
-        slice of self.phase : `numpy.ndarray`
-
-        """
-        return self.unit, self.phase[:, self.center]
-
-    @property
-    def pv(self):
-        """Return the peak-to-valley wavefront error as a `float`.
-
-        """
-        return pv(self.phase)
-
-    @property
-    def rms(self):
-        """Return the RMS wavefront error in the given OPD units as a `float`.
-
-        """
-        return rms(self.phase)
-
-    # quick-access slices, properties ------------------------------------------
-
-    # plotting -----------------------------------------------------------------
-
-    def plot2d(self, fig=None, ax=None):
-        """Create a 2D plot of the phase error of the pupil.
-
-        Parameters
-        ----------
-        fig : `matplotlib.figure.Figure`, optional:
-            Figure to draw plot in
-        ax : `matplotlib.axes.Axis`
-            Axis to draw plot in
-
-        Returns
-        -------
-        fig : `matplotlib.figure.Figure`
-            Figure containing the plot
-        ax : `matplotlib.axes.Axis`
-            Axis containing the plot
-
-        """
-        epd = self.epd
-
-        fig, ax = share_fig_ax(fig, ax)
-        im = ax.imshow(self.phase,
-                       extent=[-epd / 2, epd / 2, -epd / 2, epd / 2],
-                       cmap='RdYlBu',
-                       interpolation='lanczos',
-                       origin='lower')
-        cb = fig.colorbar(im, label=f'OPD [{self._opd_str}]', ax=ax, fraction=0.046)
-        cb.outline.set_edgecolor('k')
-        cb.outline.set_linewidth(0.5)
-        ax.set(xlabel=r'Pupil $\xi$ [mm]',
-               ylabel=r'Pupil $\eta$ [mm]')
-        return fig, ax
-
-    def plot_slice_xy(self, fig=None, ax=None):
-        """Create a plot of slices through the X and Y axes of the `Pupil`.
-
-        Parameters
-        ----------
-        fig : `matplotlib.figure.Figure`, optional
-            Figure to draw plot in
-        ax : `matplotlib.axes.Axis`
-            Axis to draw plot in
-
-        Returns
-        -------
-        fig : `matplotlib.figure.Figure`, optional
-            Figure containing the plot
-        ax : `matplotlib.axes.Axis`, optional:
-            Axis containing the plot
-
-        """
-        u, x = self.slice_x
-        _, y = self.slice_y
-
-        fig, ax = share_fig_ax(fig, ax)
-
-        x = convert_phase(x, self)
-        y = convert_phase(y, self)
-
-        ax.plot(u, x, lw=3, label='Slice X')
-        ax.plot(u, y, lw=3, label='Slice Y')
-        ax.set(xlabel=r'Pupil $\rho$ [mm]',
-               ylabel=f'OPD [{self._opd_str}]')
-        ax.legend()
-        return fig, ax
-
-    def interferogram(self, visibility=1, passes=2, fig=None, ax=None):
-        """Create an interferogram of the `Pupil`.
-
-        Parameters
-        ----------
-        visibility : `float`
-            Visibility of the interferogram
-        passes : `float`
-            Number of passes (double-pass, quadra-pass, etc.)
-        fig : `matplotlib.figure.Figure`, optional
-            Figure to draw plot in
-        ax : `matplotlib.axes.Axis`
-            Axis to draw plot in
-
-        Returns
-        -------
-        fig : `matplotlib.figure.Figure`, optional
-            Figure containing the plot
-        ax : `matplotlib.axes.Axis`, optional:
-            Axis containing the plot
-
-        """
-        epd = self.epd
-
-        fig, ax = share_fig_ax(fig, ax)
-        plotdata = (visibility * m.sin(2 * m.pi * passes * self.phase))
-        im = ax.imshow(plotdata,
-                       extent=[-epd / 2, epd / 2, -epd / 2, epd / 2],
-                       cmap='Greys_r',
-                       interpolation='lanczos',
-                       clim=(-1, 1),
-                       origin='lower')
-        fig.colorbar(im, label=r'Wrapped Phase [$\lambda$]', ax=ax, fraction=0.046)
-        ax.set(xlabel=r'Pupil $\xi$ [mm]',
-               ylabel=r'Pupil $\eta$ [mm]')
-        return fig, ax
 
     def build(self):
         """Construct a numerical model of a `Pupil`.
@@ -299,7 +139,7 @@ class Pupil(object):
             this pupil instance
 
         """
-        phase = convert_phase(self.phase, self)
+        phase = self.change_phase_unit(to='waves', inplace=False)
 
         # guard against nans in phase
         nans = ~m.isfinite(phase)
@@ -374,17 +214,6 @@ class Pupil(object):
         self.rho, self.phi = make_rho_phi_grid(self.samples, aligned='y')
         return self.rho, self.phi
 
-    def _correct_phase_units(self):
-        """Convert an expression of OPD in a unit to waves.
-
-        Returns
-        -------
-        `Pupil`
-            this pupil instance
-
-        """
-        return convert_phase(self.phase, self)
-
     def __add__(self, other):
         """Sum the phase of two pupils.
 
@@ -442,27 +271,3 @@ class Pupil(object):
         result = result._phase_to_wavefunction()
         result.mask(result._mask, result.mask_target)
         return result
-
-
-def convert_phase(array, pupil):
-    """Convert an OPD/phase map to have the same unit of expression as a pupil.
-
-    Parameters
-    ----------
-    array : `numpy.ndarray` or `float`
-        array of phase data
-    pupil : `Pupil`
-        a pupil to match the phase units to
-
-    Returns
-    -------
-    `numpy.ndarray`
-        phase-corrected array.
-
-    """
-    if pupil._opd_unit == 'microns':
-        return array / pupil.wavelength
-    elif pupil._opd_unit == 'nanometers':
-        return array / pupil.wavelength / 1e3
-    else:
-        return array

prysm/_phase.py
@@ -1,0 +1,305 @@
+"""phase basics."""
+
+from .util import share_fig_ax, pv, rms, Ra
+
+from prysm import mathops as m
+
+
+class OpticalPhase(object):
+    units = {
+        'm': 'm',
+        'meter': 'm',
+        'mm': 'mm',
+        'millimeter': 'mm',
+        'μm': 'μm',
+        'um': 'μm',
+        'micron': 'μm',
+        'micrometer': 'μm',
+        'nm': 'nm',
+        'nanometer': 'nm',
+        'Å': 'Å',
+        'aa': 'Å',
+        'angstrom': 'Å',
+        'λ': 'λ',
+        'waves': 'λ',
+        'lambda': 'λ',
+        'px': 'px',
+        'pixel': 'px',
+    }
+    unit_scales = {
+        'm': 1,
+        'mm': 1e-3,
+        'μm': 1e-6,
+        'nm': 1e-9,
+        'Å': 1e-10,
+    }
+    unit_changes = {
+        'm_mm': lambda x: 1e3,
+        'm_μm': lambda x: 1e-6,
+        'm_nm': lambda x: 1e9,
+        'm_Å': lambda x: 1e10,
+        'm_λ': lambda x: 1e6 / x,
+        'mm_mm': lambda x: 1,
+        'mm_m': lambda x: 1e-3,
+        'mm_μm': lambda x: 1e3,
+        'mm_nm': lambda x: 1e6,
+        'mm_Å': lambda x: 1e7,
+        'mm_λ': lambda x: 1e3 / x,
+        'μm_μm': lambda x: 1,
+        'μm_m': lambda x: 1e-6,
+        'μm_mm': lambda x: 1e-3,
+        'μm_um': lambda x: 1e3,
+        'μm_Å': lambda x: 1e4,
+        'μm_λ': lambda x: 1 / x,
+        'nm_nm': lambda x: 1,
+        'nm_m': lambda x: 1e-9,
+        'nm_mm': lambda x: 1e-6,
+        'nm_μm': lambda x: 1e-3,
+        'nm_Å': lambda x: 1e-1,
+        'nm_λ': lambda x: 1e-3 / x,
+        'Å_Å': lambda x: 1,
+        'Å_m': lambda x: 1e-10,
+        'Å_mm': lambda x: 1e-7,
+        'Å_μm': lambda x: 1e-4,
+        'Å_nm': lambda x: 10,
+        'Å_λ': lambda x: 1e-4 / x,
+        'λ_λ': lambda x: 1,
+        'λ_m': lambda x: 1e-6 * x,
+        'λ_mm': lambda x: 1e-3 * x,
+        'λ_um': lambda x: x,
+        'λ_nm': lambda x: 1e3 * x,
+        'λ_Å': lambda x: 1e4 * x,
+
+    }
+
+    def __init__(self, unit_x, unit_y, phase, phase_unit, spatial_unit, wavelength=None):
+        """Create a new instance of an OpticalPhase.
+
+        Note that this class is not intended to be used directly, and is meant
+        to allow shared functionality and interchange between the `Pupil` and
+        `Interferogram` classes.
+
+        Parameters
+        ----------
+        unit_x : `np.ndarray`
+            x spatial units
+        unit_y : `np.ndarray`
+            y spatial units
+        phase : `np.ndarray`
+            phase/height/opd data
+        phase_unit : `str`
+            unit used to describe the phase, see `OpticalPhase`.units
+        spatial_unit : `str`
+            unit used to describe unit_x and unit_y, see `OpticalPhase`.units
+        wavelength : `float`, optional
+            wavelength of light, in microns
+
+        """
+        self.unit_x = unit_x
+        self.unit_y = unit_y
+        self.phase = phase
+        self.wavelength = wavelength
+        self.phase_unit = self.__class__.units[phase_unit.lower()]
+        self.spatial_unit = self.__class__.units[spatial_unit.lower()]
+        self.center_x = len(self.unit_x) // 2
+        self.center_y = len(self.unit_y) // 2
+
+    @property
+    def slice_x(self):
+        """Retrieve a slice through the X axis of the phase.
+
+        Returns
+        -------
+        self.unit : `numpy.ndarray`
+            ordinate axis
+        slice of self.phase : `numpy.ndarray`
+
+        """
+        return self.unit, self.phase[self.center]
+
+    @property
+    def slice_y(self):
+        """Retrieve a slice through the Y axis of the phase.
+
+        Returns
+        -------
+        self.unit : `numpy.ndarray`
+            ordinate axis
+        slice of self.phase : `numpy.ndarray`
+
+        """
+        return self.unit, self.phase[:, self.center]
+
+    @property
+    def pv(self):
+        return pv(self.phase)
+
+    @property
+    def rms(self):
+        return rms(self.phase)
+
+    @property
+    def Ra(self):
+        return Ra(self.phase)
+
+    def change_phase_unit(self, to, inplace=False):
+        """Change the units used to describe the phase.
+
+        Parameters
+        ----------
+        to : `str`
+            new unit, a member of `OpticalPhase`.units.keys()
+        inplace : `bool`, optional
+            whether to change self.phase, if False, returns updated phase, if True, returns self.
+
+        Returns
+        -------
+        `new_phase` : `np.ndarray`
+            new phase data
+        OR
+        `self` : `OpticalPhase`
+            self
+        """
+        fctr = self.__class__.unit_changes['_'.join([self.phase_unit, self.__class__.units[to]])](self.wavelength)
+        new_phase = self.phase / fctr
+        if inplace:
+            self.phase = new_phase
+            self.phase_unit = self.__class__.units[to.lower()]
+            return self
+        else:
+            return new_phase
+
+    def change_spatial_unit(self, to, inplace=False):
+        """Change the units used to describe the spatial dimensions.
+
+        Parameters
+        ----------
+        to : `str`
+            new unit, a member of `OpticalPhase`.units.keys()
+        inplace : `bool`, optional
+            whether to change self.unit_x and self.unit_y.
+            If False, returns updated phase, if True, returns self
+
+        Returns
+        -------
+        `new_ux` : `np.ndarray`
+            new ordinate x axis
+        `new_uy` : `np.ndarray`
+            new ordinate y axis
+        OR
+        `self` : `OpticalPhase`
+            self
+
+        """
+        fctr = self.__class__.unit_changes['_'.join([self.spatial_unit, self.__class__.units[to]])](self.wavelength)
+        new_ux = self.unit_x / fctr
+        new_uy = self.unit_y / fctr
+        if inplace:
+            self.unit_x = new_ux
+            self.unit_y = new_uy
+            self.spatial_unit = self.__class__.units[to.lower()]
+            return self
+        else:
+            return new_ux, new_uy
+
+    def plot2d(self, cmap='inferno', clim=(None, None), interp_method='lanczos', fig=None, ax=None):
+        """Plot the phase in 2D.
+
+        Parameters
+        ----------
+        cmap : `str`
+            colormap to use, passed directly to matplotlib
+        interp_method : `str`
+            interpolation method to use, passed directly to matplotlib
+        fig : `matplotlib.figure.Figure`
+            Figure containing the plot
+        ax : `matplotlib.axes.Axis`
+            Axis containing the plot
+
+        Returns
+        -------
+        fig : `matplotlib.figure.Figure`
+            Figure containing the plot
+        ax : `matplotlib.axes.Axis`
+            Axis containing the plot
+
+        """
+        fig, ax = share_fig_ax(fig, ax)
+
+        im = ax.imshow(self.phase,
+                       extent=[self.unit_x[0], self.unit_x[-1], self.unit_y[0], self.unit_y[-1]],
+                       cmap=cmap,
+                       clim=clim,
+                       interpolation=interp_method)
+        fig.colorbar(im, label=f'{self.zaxis_label} [{self.phase_unit}]', ax=ax, fraction=0.046)
+        xlab = f'{self.xaxis_label} [{self.spatial_unit}]'
+        ylab = f'{self.yaxis_label} [{self.spatial_unit}]'
+        ax.set(xlabel=xlab, ylabel=ylab)
+        return fig, ax
+
+    def plot_slice_xy(self, fig=None, ax=None):
+        """Create a plot of slices through the X and Y axes of the `Pupil`.
+
+        Parameters
+        ----------
+        fig : `matplotlib.figure.Figure`, optional
+            Figure to draw plot in
+        ax : `matplotlib.axes.Axis`
+            Axis to draw plot in
+
+        Returns
+        -------
+        fig : `matplotlib.figure.Figure`, optional
+            Figure containing the plot
+        ax : `matplotlib.axes.Axis`, optional:
+            Axis containing the plot
+
+        """
+        u, x = self.slice_x
+        _, y = self.slice_y
+
+        fig, ax = share_fig_ax(fig, ax)
+
+        ax.plot(u, x, lw=3, label='Slice X')
+        ax.plot(u, y, lw=3, label='Slice Y')
+        ax.set(xlabel=r'x dimension [mm]',
+               ylabel=f'OPD [{self._opd_str}]')
+        ax.legend()
+        return fig, ax
+
+    def interferogram(self, visibility=1, passes=2, fig=None, ax=None):
+        """Create an interferogram of the `Pupil`.
+
+        Parameters
+        ----------
+        visibility : `float`
+            Visibility of the interferogram
+        passes : `float`
+            Number of passes (double-pass, quadra-pass, etc.)
+        fig : `matplotlib.figure.Figure`, optional
+            Figure to draw plot in
+        ax : `matplotlib.axes.Axis`
+            Axis to draw plot in
+
+        Returns
+        -------
+        fig : `matplotlib.figure.Figure`, optional
+            Figure containing the plot
+        ax : `matplotlib.axes.Axis`, optional:
+            Axis containing the plot
+
+        """
+        epd = self.epd
+
+        fig, ax = share_fig_ax(fig, ax)
+        plotdata = (visibility * m.sin(2 * m.pi * passes * self.phase))
+        im = ax.imshow(plotdata,
+                       extent=[-epd / 2, epd / 2, -epd / 2, epd / 2],
+                       cmap='Greys_r',
+                       interpolation='lanczos',
+                       clim=(-1, 1),
+                       origin='lower')
+        fig.colorbar(im, label=r'Wrapped Phase [$\lambda$]', ax=ax, fraction=0.046)
+        ax.set(xlabel=r'Pupil $\xi$ [mm]',
+               ylabel=r'Pupil $\eta$ [mm]')
+        return fig, ax

prysm/interferogram.py
@@ -1,56 +1,33 @@
 """tools to analyze interferometric data."""
+from ._phase import OpticalPhase
+from ._zernike import defocus
 from .io import read_zygo_dat
 from .fttools import forward_ft_unit
-from .util import share_fig_ax, pv, rms, Ra
-from ._zernike import defocus
 from .coordinates import cart_to_polar
 
 
 from prysm import mathops as m
 
 
-class Interferogram(object):
-    labels = {
-        True: {
-            'um': (r'x [$\mu m$]', r'y [$\mu m$]'),
-            'mm': ('x [mm]', 'y [mm]'),
-        },
-        False: {
-            'um': ('x [px]', 'y [px]'),
-            'mm': ('x [px]', 'y [px]'),
-        }
-    }
-    scale_map = {
-        'um': 1e6,
-        'mm': 1e3,
-    }
+class Interferogram(OpticalPhase):
     """Class containing logic and data for working with interferometric data."""
-    def __init__(self, phase, intensity=None, x=None, y=None, scale='um', meta=None):
-        self.phase = phase
-        self.intensity = intensity
-        self.scale = scale
-        self.meta = meta
+    def __init__(self, phase, intensity=None, x=None, y=None, scale='px', meta=None):
         if x is None:  # assume x, y given together
-            self.x = m.arange(phase.shape[1])
-            self.y = m.arange(phase.shape[0])
-            self._realxy = False
+            x = m.arange(phase.shape[1])
+            y = m.arange(phase.shape[0])
+            scale = 'px'
             self.lateral_res = None
-        else:
-            self.x, self.y = x, y
-            self._realxy = True
-            self.sample_spacing = x[1] - x[0]
 
-    @property
-    def pv(self):
-        return pv(self.phase)
+        super().__init__(unit_x=x, unit_y=y, phase=phase,
+                         wavelength=meta.get('wavelength'), phase_unit='nm', spatial_unit='m')
 
-    @property
-    def rms(self):
-        return rms(self.phase)
-
-    @property
-    def Ra(self):
-        return Ra(self.phase)
+        self.xaxis_label = 'X'
+        self.yaxis_label = 'Y'
+        self.zaxis_label = 'Height'
+        self.intensity = intensity
+        self.meta = meta
+        if scale is not 'px':
+            self.change_spatial_unit(to=scale, inplace=True)
 
     @property
     def dropout_percentage(self):
@@ -64,7 +41,7 @@ class Interferogram(object):
         left, right = nanrows.argmax(), nanrows[::-1].argmax()
         top, bottom = nancols.argmax(), nancols[::-1].argmax()
         self.phase = self.phase[left:-right, top:-bottom]
-        self.y, self.x = self.y[left:-right], self.x[top:-bottom]
+        self.unit_y, self.unit_x = self.unit_y[left:-right], self.unit_x[top:-bottom]
         return self
 
     def remove_piston(self):
@@ -74,7 +51,7 @@ class Interferogram(object):
 
     def remove_tiptilt(self):
         """Remove tip/tilt from the data by least squares fitting and subtracting a plane."""
-        plane = fit_plane(self.x, self.y, self.phase)
+        plane = fit_plane(self.unit_x, self.unit_y, self.phase)
         self.phase -= plane
         return self
 
@@ -122,40 +99,6 @@ class Interferogram(object):
         self.phase = new_phase
         return self
 
-    def plot2d(self, cmap='inferno', clim=(None, None), interp_method='lanczos', fig=None, ax=None):
-        """Plot the data in 2D.
-
-        Parameters
-        ----------
-        cmap : `str`
-            colormap to use, passed directly to matplotlib
-        interp_method : `str`
-            interpolation method to use, passed directly to matplotlib
-        fig : `matplotlib.figure.Figure`
-            Figure containing the plot
-        ax : `matplotlib.axes.Axis`
-            Axis containing the plot
-
-        Returns
-        -------
-        fig : `matplotlib.figure.Figure`
-            Figure containing the plot
-        ax : `matplotlib.axes.Axis`
-            Axis containing the plot
-
-        """
-        fig, ax = share_fig_ax(fig, ax)
-
-        im = ax.imshow(self.phase,
-                       extent=[self.x[0], self.x[-1], self.y[0], self.y[-1]],
-                       cmap=cmap,
-                       clim=clim,
-                       interpolation=interp_method)
-        fig.colorbar(im, label='Height [nm]', ax=ax, fraction=0.046)
-        xlab, ylab = self.__class__.labels[self._realxy][self.scale]
-        ax.set(xlabel=xlab, ylabel=ylab)
-        return fig, ax
-
     @staticmethod
     def from_zygo_dat(path, multi_intensity_action='first', scale='um'):
         """Create a new interferogram from a zygo dat file.
@@ -176,7 +119,7 @@ class Interferogram(object):
 
         """
         zydat = read_zygo_dat(path, multi_intensity_action=multi_intensity_action)
-        res = zydat['meta']['lateral_resolution'] * Interferogram.scale_map[scale.lower()]
+        res = zydat['meta']['lateral_resolution']  # meters
         phase = zydat['phase']
         return Interferogram(phase=phase, intensity=zydat['intensity'],
                              x=m.arange(phase.shape[1]) * res, y=m.arange(phase.shape[0]) * res,

commit 8b180c43bd4fa7709dbcc99fe85a9b8f59a0a71b

correct bug introduced in ac508863f06dc206c80fb4ca0e1412a1a9428209 that caused wrong pupil dimensions

prysm/pupil.py
@@ -89,8 +89,8 @@ class Pupil(OpticalPhase):
 
         """
         self.epd = epd
-        ux = m.linspace(-epd//2, epd//2, samples)
-        uy = m.linspace(-epd//2, epd//2, samples)
+        ux = m.linspace(-epd / 2, epd / 2, samples)
+        uy = m.linspace(-epd / 2, epd / 2, samples)
         self.samples = samples
         self.sample_spacing = ux[1] - ux[0]
         super().__init__(unit_x=ux, unit_y=uy, phase=None,