be0127fdb3ec3d99c57cbba7ea859d4e69d6b8e0
Brandon Dube <brandondube@gmail.com>
2019-02-10 11:44:31 -0500

    + several grayscale sample images

--- prysm/sample_data.py
+++ prysm/sample_data.py
@@ -9,27 +9,34 @@ root = baselocal.parent / 'sampledata'
 root.mkdir(exist_ok=True)
 
 
+def fetch_if_not_present(local, remote):
+    """Fetch a file from github if it is not present in the local filesystem."""
+    if not local.exists():
+        with urlopen(remote) as response, open(local, 'wb') as fid:
+            shutil.copyfileobj(response, fid)
+
+    return local
+
+
 class SampleFiles:
     """Sample files for prysm."""
     dat = 'valid_zygo_dat_file.dat'
     mtfvfvf = 'valid_sample_MTFvFvF_Sag.txt'
     mtfvf = 'valid_sample_trioptics_mtf_vs_field.mht'
 
-    def __call__(self, dtype):
+    def __call__(self, dtype_or_filename):
         """Get the path of a sample file."""
-        dtype = dtype.lower()
-        if hasattr(self, dtype):
-            filename = getattr(self, dtype)
-            local = (root / getattr(self, dtype)).absolute()
+        dtype_or_filename = dtype_or_filename.lower()
+        if hasattr(self, dtype_or_filename):
+            filename = getattr(self, dtype_or_filename)
+            local = (root / getattr(self, dtype_or_filename)).absolute()
             remote = baseremote + filename
-            if not local.exists():
-                with urlopen(remote) as response, open(local, 'wb') as fid:
-                    shutil.copyfileobj(response, fid)
-
-            return local
+            return fetch_if_not_present(local, remote)
 
         else:
-            raise ValueError('invalid sample filetype requested.')
+            local = root / dtype_or_filename
+            remote = baseremote + filename
+            return fetch_if_not_present(local, remote)
 
 
 sample_files = SampleFiles()
