17b3ef13482a580210547296aa3cf147a1f987a1
Brandon Dube <brandondube@gmail.com>
2019-02-28 11:11:44 -0500

    update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'prysm'
-copyright = '2017-2018, Brandon Dube'
+copyright = '2017-2019, Brandon Dube'
 author = 'Brandon Dube'
 
 # The version info for the project you're documenting, acts as replacement for
