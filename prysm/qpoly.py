620664c1ea70b42eef3c36c174df59bebb84d287
Brandon Dube <brandondube@gmail.com>
2019-07-10 20:41:05 -0400

    even faster Q polynomials and variable maximum coordinate

--- prysm/qpoly.py
+++ prysm/qpoly.py
@@ -1,6 +1,5 @@
 """Tools for working with Q (Forbes) polynomials."""
 from functools import lru_cache
-from collections import defaultdict
 
 from .conf import config
 from .pupil import Pupil
@@ -8,7 +7,7 @@ from .mathops import engine as e
 from .coordinates import make_rho_phi_grid
 
 
-def qbfs_recurrence_P(n, x, Pnm1=None, Pnm2=None):
+def qbfs_recurrence_P(n, x, Pnm1=None, Pnm2=None, recursion_coef=None):
     if n == 0:
         return 2
     elif n == 1:
@@ -18,25 +17,31 @@ def qbfs_recurrence_P(n, x, Pnm1=None, Pnm2=None):
             Pnm1 = qbfs_recurrence_P(n - 1, x)
         if Pnm2 is None:
             Pnm2 = qbfs_recurrence_P(n - 2, x)
-        return (2 - 4 * x) * Pnm1 - Pnm2
+        if recursion_coef is None:
+            recursion_coef = 2 - 4 * x
+
+        return recursion_coef * Pnm1 - Pnm2
 
 
-def qbfs_recurrence_Q(n, x, Pn=None, Pnm1=None, Pnm2=None, Qnm1=None, Qnm2=None):
+def qbfs_recurrence_Q(n, x, Pn=None, Pnm1=None, Pnm2=None, Qnm1=None, Qnm2=None, recursion_coef=None):
     if n == 0:
-        return e.ones_like(x, dtype=config.precision)
+        return e.ones_like(x)
     elif n == 1:
         return 1 / e.sqrt(19) * (13 - 16 * x)
     else:
+        # allow passing of cached results
+        if Pnm2 is None:
+            Pnm2 = qbfs_recurrence_P(n - 2, x, recursion_coef=recursion_coef)
+        if Pnm1 is None:
+            Pnm1 = qbfs_recurrence_P(n - 1, x, Pnm2=Pnm2, recursion_coef=recursion_coef)
         if Pn is None:
-            if Pnm1 is None:
-                Pnm1 = qbfs_recurrence_P(n - 1, x)
-            if Pnm2 is None:
-                Pnm2 = qbfs_recurrence_P(n - 2, x)
-            Pn = qbfs_recurrence_P(n, x, Pnm1=Pnm1, Pnm2=Pnm2)
-        if Qnm1 is None:
-            Qnm1 = qbfs_recurrence_Q(n - 1, x)
+            Pn = qbfs_recurrence_P(n, x, Pnm1=Pnm1, Pnm2=Pnm2, recursion_coef=recursion_coef)
         if Qnm2 is None:
-            Qnm2 = qbfs_recurrence_Q(n - 2, x)
+            Qnm2 = qbfs_recurrence_Q(n - 2, x, Pn=Pn, Pnm1=Pnm1, Pnm2=Pnm2, recursion_coef=recursion_coef)
+        if Qnm1 is None:
+            Qnm1 = qbfs_recurrence_Q(n - 1, x, Pn=Pn, Pnm1=Pnm1, Pnm2=Pnm2, Qnm2=Qnm2, recursion_coef=recursion_coef)
+
+        # now calculate the three-term recursion
         term1 = Pn
         term2 = g(n - 1) * Qnm1
         term3 = h(n - 2) * Qnm2
@@ -80,66 +85,72 @@ class QBFSCache(object):
     """Cache of Qbfs terms evaluated over the unit circle."""
     def __init__(self):
         """Create a new QBFSCache instance."""
-        self.Qs = defaultdict(dict)
-        self.Ps = defaultdict(dict)
-        self.grids = dict()
+        self.Qs = {}
+        self.Ps = {}
+        self.grids = {}
         self.Pm = None
         self.Pnm2, self.Pnm1 = None, None
         self.Qnm2, self.Qnm1 = None, None
 
-    def get_QBFS(self, m, samples):
+    def get_QBFS(self, m, samples, rho_max=1):
         """Get an array of phase values for a given index, and number of samples."""
+        key = self.make_key(m=m, samples=samples, rho_max=rho_max)
         try:
-            Qm = self.Qs[m][samples]
+            Qm = self.Qs[key]
         except KeyError:
-            rho = self.get_grid(samples)
-            self.Pm = self.get_PBFS(m, samples)
+            rho = self.get_grid(samples, rho_max=rho_max)
+            self.Pm = self.get_PBFS(m=m, samples=samples, rho_max=rho_max)
             if m > 2:
-                self.Pnm2 = self.get_PBFS(m - 2, samples)
-                self.Pnm1 = self.get_PBFS(m - 1, samples)
-                self.Qnm2 = self.get_QBFS(m - 2, samples)
-                self.Qnm1 = self.get_QBFS(m - 1, samples)
+                self.Pnm2 = self.get_PBFS(m=m - 2, samples=samples, rho_max=rho_max)
+                self.Pnm1 = self.get_PBFS(m=m - 1, samples=samples, rho_max=rho_max)
+                self.Qnm2 = self.get_QBFS(m=m - 2, samples=samples, rho_max=rho_max)
+                self.Qnm1 = self.get_QBFS(m=m - 1, samples=samples, rho_max=rho_max)
 
             Qm = qbfs_recurrence_Q(m, rho, Pn=self.Pm, Pnm1=self.Pnm1, Pnm2=self.Pnm2,
                                    Qnm1=self.Qnm1, Qnm2=self.Qnm2)
-            self.Qs[m][samples] = Qm
+            self.Qs[key] = Qm
 
         return Qm
 
-    def get_PBFS(self, m, samples):
+    def get_PBFS(self, m, samples, rho_max=1):
         """Get an array of P values for a given index."""
+        key = self.make_key(m=m, samples=samples, rho_max=rho_max)
         try:
-            Pm = self.Ps[m][samples]
+            Pm = self.Ps[key]
         except KeyError:
-            rho = self.get_grid(samples)
+            rho = self.get_grid(samples, rho_max=rho_max)
             if m > 2:
-                self.Pnm2 = self.get_PBFS(m - 2, samples)
-                self.Pnm1 = self.get_PBFS(m - 1, samples)
+                self.Pnm2 = self.get_PBFS(m - 2, samples=samples, rho_max=rho_max)
+                self.Pnm1 = self.get_PBFS(m - 1, samples=samples, rho_max=rho_max)
 
             Pm = qbfs_recurrence_P(m, rho, Pnm1=self.Pnm1, Pnm2=self.Pnm2)
-            self.Ps[m][samples] = Pm
+            self.Ps[key] = Pm
 
         return Pm
 
-    def get_grid(self, samples):
+    def get_grid(self, samples, rho_max=1):
         """Get a grid of rho coordinates for a given number of samples."""
+        key = self.make_key(None, samples=samples, rho_max=rho_max)[1:]
         try:
-            rho = self.grids[samples]
+            rho = self.grids[key]
         except KeyError:
-            rho, _ = make_rho_phi_grid(samples, aligned='y')
-            self.grids[samples] = rho
+            rho, _ = make_rho_phi_grid(samples, aligned='y', radius=rho_max)
+            self.grids[key] = rho
 
         return rho
 
-    def __call__(self, number, samples):
+    def __call__(self, m, samples, rho_max=1):
         """Get an array of sag values for a given index, norm, and number of samples."""
-        return self.get_QBFS(number, samples)
+        return self.get_QBFS(m=m, samples=samples, rho_max=rho_max)
+
+    def make_key(self, m, samples, rho_max):
+        return (m, samples, rho_max)
 
     def clear(self, *args):
         """Empty the cache."""
-        self.Qs = defaultdict(dict)
-        self.Ps = defaultdict(dict)
-        self.grids = dict()
+        self.Qs = {}
+        self.Ps = {}
+        self.grids = {}
 
 
 QBFScache = QBFSCache()
