337cb1b124e7d4a74cc928385f3d17f6fd66953e
Brandon Dube <brandondube@gmail.com>
2019-08-03 18:40:38 -0700

    some Q poly rearrangement.  Need to make something to allow cascades of transformations

--- prysm/qpoly.py
+++ prysm/qpoly.py
@@ -1,10 +1,10 @@
 """Tools for working with Q (Forbes) polynomials."""
-from functools import lru_cache
 
 from .conf import config
 from .pupil import Pupil
 from .mathops import engine as e
-from .coordinates import make_rho_phi_grid
+from .coordinates import gridcache
+from .jacobi import jacobi
 
 
 def qbfs_recurrence_P(n, x, Pnm1=None, Pnm2=None, recursion_coef=None):
@@ -52,7 +52,6 @@ def qbfs_recurrence_Q(n, x, Pn=None, Pnm1=None, Pnm2=None, Qnm1=None, Qnm2=None,
         return numerator / denominator
 
 
-@lru_cache()
 def g_qbfs(n_minus_1):
     """g(m-1) from oe-18-19-19700 eq. (A.15)"""
     if n_minus_1 == 0:
@@ -62,14 +61,12 @@ def g_qbfs(n_minus_1):
         return - (1 + g_qbfs(n_minus_2) * h_qbfs(n_minus_2)) / f_qbfs(n_minus_1)
 
 
-@lru_cache()
 def h_qbfs(n_minus_2):
     """h(m-2) from oe-18-19-19700 eq. (A.14)"""
     n = n_minus_2 + 2
     return -n * (n - 1) / (2 * f_qbfs(n_minus_2))
 
 
-@lru_cache()
 def f_qbfs(n):
     """f(m) from oe-18-19-19700 eq. (A.16)"""
     if n == 0:
@@ -87,11 +84,11 @@ class QBFSCache(object):
     """Cache of Qbfs terms evaluated over the unit circle.
 
     Note that the .grids attribute stores only radial coordinates, and they are stored in squared form."""
-    def __init__(self):
+    def __init__(self, gridcache=gridcache):
         """Create a new QBFSCache instance."""
         self.Qs = {}
         self.Ps = {}
-        self.grids = {}
+        self.gridcache = gridcache
 
     def get_QBFS(self, m, samples, rho_max=1):
         """Get an array of phase values for a given index, and number of samples."""
@@ -136,15 +133,7 @@ class QBFSCache(object):
 
     def get_grid(self, samples, rho_max=1):
         """Get a grid of rho coordinates for a given number of samples."""
-        key = self.make_key(None, samples=samples, rho_max=rho_max)[1:]
-        try:
-            rho = self.grids[key]
-        except KeyError:
-            rho, _ = make_rho_phi_grid(samples, aligned='y', radius=rho_max)
-            rho = rho ** 2
-            self.grids[key] = rho
-
-        return rho
+        return self.gridcache(samples=samples, radius=rho_max, r='r -> r^2')['r']
 
     def __call__(self, m, samples, rho_max=1):
         """Get an array of sag values for a given index, norm, and number of samples."""
@@ -173,6 +162,94 @@ class QBFSCache(object):
 
 QBFScache = QBFSCache()
 config.chbackend_observers.append(QBFScache.clear)
+
+
+# Qcon is defined as:
+# r^4 * P_m(0,4)(2x-1)
+# with x = r^2
+
+
+def qcon_recurrence(n, x, Pnm1=None, Pnm2=None):
+    return jacobi(n, x=x, alpha=0, beta=4, Pnm1=Pnm1, Pnm2=Pnm2)
+
+
+class QCONCache(object):
+    """Cache of Qcon terms evaluated over the unit circle."""
+    def __init__(self, gridcache=gridcache):
+        """Create a new QCONCache instance."""
+        self.Qs = {}
+        self.Ps = {}
+        self.gridcache = gridcache
+
+    def get_QCON(self, m, samples, rho_max=1):
+        """Get an array of phase values for a given index, and number of samples."""
+        # TODO: update
+        key = self.make_key(m=m, samples=samples, rho_max=rho_max)
+        try:
+            Qm = self.Qs[key]
+        except KeyError:
+            rho = self.get_grid(samples, rho_max=rho_max)
+            if m > 2:
+                Pnm2 = self.get_PJAC(m=m - 2, samples=samples, rho_max=rho_max)
+                Pnm1 = self.get_PJAC(m=m - 1, samples=samples, rho_max=rho_max)
+            else:
+                Pnm1, Pnm2 = None, None
+
+            Qm = qcon_recurrence(m, rho, Pnm1=Pnm1, Pnm2=Pnm2)
+            self.Qs[key] = Qm
+
+        return Qm
+
+    def get_PJAC(self, m, samples, rho_max=1):
+        """Get an array of P_n^(0,4) values for a given index."""
+        # TODO: update
+        key = self.make_key(m=m, samples=samples, rho_max=rho_max)
+        try:
+            Pm = self.Ps[key]
+
+        except KeyError:
+            rho = self.get_grid(samples, rho_max=rho_max)
+            if m > 2:
+                Pnm2 = self.get_PJAC(m - 2, samples=samples, rho_max=rho_max)
+                Pnm1 = self.get_PJAC(m - 1, samples=samples, rho_max=rho_max)
+            else:
+                Pnm1, Pnm2 = None, None
+
+            Pm = jacobi(n=m, x=rho, alpha=0, beta=4, Pnm1=Pnm1, Pnm2=Pnm2)
+            self.Ps[key] = Pm
+
+        return Pm
+
+    def get_grid(self, samples, rho_max=1):
+        """Get a grid of rho coordinates for a given number of samples."""
+        return self.gridcache(samples=samples, radius=rho_max, r='r -> r^2')['r']
+
+    def __call__(self, m, samples, rho_max=1):
+        """Get an array of sag values for a given index, norm, and number of samples."""
+        return self.get_QCON(m=m, samples=samples, rho_max=rho_max)
+
+    def make_key(self, m, samples, rho_max):
+        """Generate a key into the cache dictionaries."""
+        return (m, samples, rho_max)
+
+    def clear(self, *args):
+        """Empty the cache."""
+        self.Qs = {}
+        self.Ps = {}
+        self.grids = {}
+
+    @property
+    def nbytes(self):
+        n = 0
+        for key in self.Qs:
+            n += self.Qs[key].nbytes
+            n += self.Ps[key].nbytes
+
+        return n
+
+
+QCONcache = QCONCache()
+config.chbackend_observers.append(QCONcache.clear)
 
 
 # Note that this class doesn't implement _name and other RichData requirements
@@ -227,30 +304,27 @@ class QBFSSag(QPolySag1D):
 
         """
         super().build()
-        r2 = self._cache.get_grid(self.samples)
-        coef = r2 * (1 - r2)
+        coef = self._cache.gridcache(samples=self.samples, radius=1, r='r -> 2r^2 - 1')['r']
         self.phase *= coef
 
 
-def a_qcon(n):
-    """a(n) from oe-18-13-13851 eq. (5.2a)."""
-    numerator = (2 * n + 5) * (n ** 2 + 5 * n + 10)
-    denominator = (n + 1) * (n + 5) * (n + 5)
-    return numerator / denominator
+class QCONSag(QPolySag1D):
+    _name = 'Qcon'
+    _cache = QCONcache
+    """Qcon aspheric surface sag, excluding base sphere."""
 
+    def build(self):
+        """Use the aspheric coefficients stored in this class instance to build a sag model.
 
-def b_qcon(n):
-    """b(n) from oe-18-13-13851 eq. (5.2b)."""
-    numerator = 2 * (n + 3) * (2 * n + 5)
-    denominator = (n + 1) * (n + 5)
-    return numerator / denominator
+        Returns
+        -------
+        self : `QCONSag`
+            this QCONSag instance`
 
-
-def c_qcon(n):
-    """c(n) from oe-18-13-13851 eq. (5.2c)."""
-    numerator = (n + 3) * (n + 4) * n
-    denominator = (n + 1) * (n + 2) * (n + 5)
-    return numerator / denominator
+        """
+        super().build()
+        coef = self._cache.gridcache(samples=self.samples, radius=1, r='r -> r^4')['r']
+        self.phase *= coef
 
 
 def a_zernike(m, n):
@@ -275,134 +349,3 @@ def c_zernike(m, n):
     numerator = (s + 2) * (s - n) * n
     denominator = (n + 1) * (s - n + 1) * s
     return numerator / denominator
-
-
-def qcon_recurrence(n, x, qnm1=None, qnm2=None):
-    from scipy.special import jacobi
-    jacpoly = jacobi(n, 0, 4)
-    return jacpoly(x)
-
-
-class QCONCache(object):
-    """Cache of Qcon terms evaluated over the unit circle.
-
-    Note that the .grids attribute stores only radial coordinates, and they are stored as:
-    x => x^2
-      => 2x - 1.
-
-    This means that all of the appropriate coordinate transformations are applied only once, instead
-    of once in the generation of each term."""
-    def __init__(self):
-        """Create a new QCONCache instance."""
-        self.Qs = {}
-        self.Ps = {}
-        self.grids = {}
-
-    def get_QCON(self, m, samples, rho_max=1):
-        """Get an array of phase values for a given index, and number of samples."""
-        # TODO: update
-        key = self.make_key(m=m, samples=samples, rho_max=rho_max)
-        try:
-            Qm = self.Qs[key]
-        except KeyError:
-            rho = self.get_grid(samples, rho_max=rho_max)
-            Pm = self.get_PBFS(m=m, samples=samples, rho_max=rho_max)
-            if m > 2:
-                Pnm2 = self.get_PBFS(m=m - 2, samples=samples, rho_max=rho_max)
-                Pnm1 = self.get_PBFS(m=m - 1, samples=samples, rho_max=rho_max)
-                Qnm2 = self.get_QBFS(m=m - 2, samples=samples, rho_max=rho_max)
-                Qnm1 = self.get_QBFS(m=m - 1, samples=samples, rho_max=rho_max)
-            else:
-                Pnm1, Pnm2, Qnm1, Qnm2 = None, None, None, None
-
-            Qm = qbfs_recurrence_Q(m, rho, Pn=Pm, Pnm1=Pnm1, Pnm2=Pnm2,
-                                   Qnm1=Qnm1, Qnm2=Qnm2)
-            self.Qs[key] = Qm
-
-        return Qm
-
-    def get_PBFS(self, m, samples, rho_max=1):
-        """Get an array of P values for a given index."""
-        # TODO: update
-        key = self.make_key(m=m, samples=samples, rho_max=rho_max)
-        try:
-            Pm = self.Ps[key]
-
-        except KeyError:
-            rho = self.get_grid(samples, rho_max=rho_max)
-            if m > 2:
-                Pnm2 = self.get_PBFS(m - 2, samples=samples, rho_max=rho_max)
-                Pnm1 = self.get_PBFS(m - 1, samples=samples, rho_max=rho_max)
-            else:
-                Pnm1, Pnm2 = None, None
-
-            Pm = qbfs_recurrence_P(m, rho, Pnm1=Pnm1, Pnm2=Pnm2)
-            self.Ps[key] = Pm
-
-        return Pm
-
-    def get_grid(self, samples, rho_max=1):
-        """Get a grid of rho coordinates for a given number of samples."""
-        key = self.make_key(None, samples=samples, rho_max=rho_max)[1:]
-        try:
-            rho = self.grids[key]
-        except KeyError:
-            rho, _ = make_rho_phi_grid(samples, aligned='y', radius=rho_max)
-            rho = rho ** 2
-            rho = 2 * rho - 1
-            self.grids[key] = rho
-
-        return rho
-
-    def __call__(self, m, samples, rho_max=1):
-        """Get an array of sag values for a given index, norm, and number of samples."""
-        return self.get_QCON(m=m, samples=samples, rho_max=rho_max)
-
-    def make_key(self, m, samples, rho_max):
-        """Generate a key into the cache dictionaries."""
-        return (m, samples, rho_max)
-
-    def clear(self, *args):
-        """Empty the cache."""
-        self.Qs = {}
-        self.Ps = {}
-        self.grids = {}
-
-    @property
-    def nbytes(self):
-        n = 0
-        for key in self.Qs:
-            n += self.Qs[key].nbytes
-            n += self.Ps[key].nbytes
-            n += self.grids[key[1:]].nbytes
-
-        return n
-
-
-QCONcache = QCONCache()
-config.chbackend_observers.append(QCONcache.clear)
-
-
-class QCONSag(QPolySag1D):
-    _name = 'Qcon'
-    _cache = QCONcache
-    """Qcon aspheric surface sag, excluding base sphere."""
-
-    def build(self):
-        """Use the aspheric coefficients stored in this class instance to build a sag model.
-
-        Returns
-        -------
-        self : `QCONSag`
-            this QCONSag instance`
-
-        """
-        super().build()
-        # todo, update this.  Want r^4, but cache grid is stored as a more complicated
-        # transformation.  Maybe store grid twice, once as r or r^2, and another as the
-        # full transformation?
-
-        # the code below is r^2, regardless.
-        r2 = self._cache.get_grid(self.samples)
-        coef = r2 * (1 - r2)
-        self.phase *= coef
