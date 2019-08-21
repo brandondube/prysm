3fec70ea236b34651679b0fd493026a4194a550e
Brandon Dube <brandondube@gmail.com>
2019-08-20 20:55:14 -0700

    + degredations test for jitter nonanalytic

--- tests/test_degredations.py
+++ tests/test_degredations.py
@@ -10,3 +10,8 @@ def test_smear():
 def test_jitter():
     jt = degredations.Jitter(1)
     assert jt.analytic_ft(0, 0) == 1
+
+
+def test_jitter_with_spatial_has_bright_origin():
+    jt = degredations.Jitter(scale=2, samples=64, sample_spacing=0.5)
+    assert jt.data[32, 32] > 0.1
