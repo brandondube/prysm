2e7a0eaa6a2d3cff80f4a762f2bdb22832d450a5
Brandon Dube <brandondube@gmail.com>
2020-01-12 16:24:59 -0800

    update copyrights and incl 0.17.2 in release tree

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'prysm'
-copyright = '2017-2019, Brandon Dube'
+copyright = '2017-2020, Brandon Dube'
 author = 'Brandon Dube'
 
 # The version info for the project you're documenting, acts as replacement for
