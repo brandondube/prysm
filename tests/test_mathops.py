2d32b6f5dff25075594bb8556bb1e227d31dafeb
Brandon Dube <brandondube@gmail.com>
2020-02-08 16:24:31 -0800

    remove test that has no meaning post-numba

--- tests/test_mathops.py
+++ tests/test_mathops.py
@@ -14,26 +14,6 @@ def sample_data_2d():
     return np.random.rand(TEST_ARR_SIZE, TEST_ARR_SIZE)
 
 
-def test_mathops_handles_own_jit_and_vectorize_definitions():
-    from importlib import reload
-    from unittest import mock
-
-    class FakeNumba():
-        __version__ = '0.35.0'
-
-    with mock.patch.dict('sys.modules', {'numba': FakeNumba()}):
-        reload(mathops)  # may have side effect of disabling numba for downstream tests.
-
-        def foo():
-            pass
-
-        foo_jit = mathops.jit(foo)
-        foo_vec = mathops.vectorize(foo)
-
-        assert foo_jit == foo
-        assert foo_vec == foo
-
-
 # below here, tests purely for function not accuracy
 def test_fft2(sample_data_2d):
     result = mathops.engine.fft.fft2(sample_data_2d)
