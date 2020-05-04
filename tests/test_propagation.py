40951250bc407a491e6d6a9adf4ebde7f1389ffc
Brandon Dube <brandondube@gmail.com>
2020-05-03 17:55:03 -0700

    add equivalency tests for mdft and fft based propagations
    use wavefront API to create higher coverage in fewer tests

--- tests/test_propagation.py
+++ tests/test_propagation.py
@@ -7,6 +7,9 @@ from prysm import propagation
 from prysm.wavelengths import HeNe
 
 
+SAMPLES = 32
+
+
 @pytest.mark.parametrize('dzeta', [1 / 128.0, 1 / 256.0, 11.123 / 128.0, 1e10 / 2048.0])
 def test_psf_to_pupil_sample_inverts_pupil_to_psf_sample(dzeta):
     samples, wvl, efl = 128, 0.55, 10
@@ -20,3 +23,29 @@ def test_obj_oriented_wavefront_focusing_reverses():
     wf = propagation.Wavefront(x=x, y=y, fcn=z, wavelength=HeNe)
     wf2 = wf.focus(1, 1).unfocus(1, 1)  # first is efl, meaningless.  second is Q, we neglect padding at the moment
     assert np.allclose(wf.fcn, wf2.fcn)
+
+
+def test_unfocus_fft_mdft_equivalent_Wavefront():
+    x = y = np.linspace(-1, 1, SAMPLES)
+    z = np.random.rand(SAMPLES, SAMPLES)
+    wf = propagation.Wavefront(x=x, y=y, fcn=z, wavelength=HeNe, space='psf')
+    unfocus_fft = wf.unfocus(Q=2, efl=1)
+    unfocus_mdft = wf.unfocus_fixed_sampling(
+        efl=1,
+        sample_spacing=unfocus_fft.sample_spacing,
+        samples=unfocus_fft.samples_x)
+
+    assert np.allclose(unfocus_fft.data, unfocus_mdft.data)
+
+
+def test_focus_fft_mdft_equivalent_Wavefront():
+    x = y = np.linspace(-1, 1, SAMPLES)
+    z = np.random.rand(SAMPLES, SAMPLES)
+    wf = propagation.Wavefront(x=x, y=y, fcn=z, wavelength=HeNe, space='pupil')
+    unfocus_fft = wf.focus(Q=2, efl=1)
+    unfocus_mdft = wf.focus_fixed_sampling(
+        efl=1,
+        sample_spacing=unfocus_fft.sample_spacing,
+        samples=unfocus_fft.samples_x)
+
+    assert np.allclose(unfocus_fft.data, unfocus_mdft.data)
