bc7f9fa4af2f06368000b2400c713b91e0ae2ff1
Brandon Dube <brandondube@gmail.com>
2020-05-16 21:13:10 -0700

    + size tests

--- tests/test_psf.py
+++ tests/test_psf.py
@@ -20,6 +20,15 @@ def tpsf():
 
 
 @pytest.fixture
+def tpsf_dense():
+    x = y = np.linspace(-LIM/4, LIM/4, SAMPLES*8)
+    xx, yy = np.meshgrid(x, y)
+    rho, phi = cart_to_polar(xx, yy)
+    dat = psf.airydisk(rho, 10, 0.55)
+    return psf.PSF(data=dat, x=x, y=y)
+
+
+@pytest.fixture
 def tpsf_mutate():
     x = y = np.linspace(-LIM, LIM, SAMPLES)
     xx, yy = np.meshgrid(x, y)
@@ -79,3 +88,21 @@ def test_coherent_propagation_is_used_in_object_oriented_api():
     p = Pupil()
     ps = psf.PSF.from_pupil(p, 1, incoherent=False)
     assert ps.data.dtype == np.complex128
+
+
+def test_size_estimation_accurate(tpsf_dense):
+    # tpsf is F/10 at lambda = 0.55 microns, so the size parameters are:
+    # FWHM
+    # 1.22 * .55 * 10 = 6.71 um
+    # the 1/e^2 width is about the same as the airy radius
+    tpsf = tpsf_dense
+    true_airy_radius = 1.22 * .55 * 10
+    true_fwhm = 1.028 * .55 * 10
+    fwhm = tpsf.fwhm()
+    one_over_e = tpsf.one_over_e()
+    one_over_esq = tpsf.one_over_e2()
+    print(fwhm, one_over_e, one_over_esq)
+    assert fwhm == pytest.approx(true_fwhm, abs=1)
+    assert one_over_e == pytest.approx(true_airy_radius/2, abs=0.1)
+    assert one_over_esq == pytest.approx(true_airy_radius/2*1.414, abs=.2)  # sqrt(2) is an empirical fudge factor.
+    # TODO: find a better test for 1/e^2
