00e3b6440adcfb165c75754c7f8560fe0e7846a6
Brandon Dube <brandondube@gmail.com>
2021-04-18 10:03:23 -0700

    io/read_zygo_datx: fix binary string decoding issues

--- prysm/io.py
+++ prysm/io.py
@@ -613,10 +613,9 @@ def read_zygo_datx(file):
         # get a little metadata
         no_data = phase_obj.attrs['No Data'][0]
         wvl = phase_obj.attrs['Wavelength'][0] * 1e9  # Zygo stores wavelength in meters, we want output in nanometers
-        punit = str(phase_obj.attrs['Unit'][0])[2:-1]  # this for some reason is "b'Fringes'", need to slice off b' and '
+        punit = phase_obj.attrs['Unit'][0].decode('UTF-8')  # this for some reason is "b'Fringes'", need to slice off b' and '
         scale_factor = phase_obj.attrs['Interferometric Scale Factor']
         obliquity = phase_obj.attrs['Obliquity Factor']
-
         # get the phase and process it as required
         phase = np.flipud(f['Data']['Surface'][phase_key][()])
         # step 1, flip (above)
@@ -654,7 +653,7 @@ def read_zygo_datx(file):
             elif key in ['Property Bag List', 'Group Number', 'TextCount']:
                 continue  # h5py particulars
             if value.dtype == 'object':
-                value = str(value[0])  # object dtype is a string
+                value = value[0].decode('UTF-8')  # object dtype is a string
             elif value.dtype in ['uint8', 'int32']:
                 value = int(value[0])
             elif value.dtype in ['float64']:
