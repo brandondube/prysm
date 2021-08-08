6b6f7bffeefc09434a847142cc9cf3f4f8c7442d
Brandon <brandondube@gmail.com>
2021-08-07 18:39:57 -0700

    mathops: simplify backend interchange system

--- prysm/mathops.py
+++ prysm/mathops.py
@@ -2,150 +2,20 @@
 import numpy as np
 from scipy import ndimage, interpolate, special, fft
 
-# it would be less code to have one class Engine, this way is perhaps clearer.
-# this may be revisited in the future
 
-
-class NDImageEngine:
-    """An engine which allows scipy.ndimage to be redirected to another lib at runtime."""
-
-    def __init__(self, ndimage=ndimage):
-        """Create a new scipy engine.
-
-        Parameters
-        ----------
-        ndimage : `module`
-            a python module, with the same API as scipy.ndimage
-        interpolate : `module`
-            a python module, with the same API as scipy.interpolate
-        special : `module`
-            a python module, with the same API as scipy.special
-
-        """
-        self.ndimage = ndimage
+class BackendShim:
+    """A shim that allows a backend to be swapped at runtime."""
+    def __init__(self, src):
+        self.__src = src
 
     def __getattr__(self, key):
-        """Get attribute.
+        return getattr(self.__src, key)
 
-        Parameters
-        ----------
-        key : `str`
-            attribute name
-
-        """
-        return getattr(self.ndimage, key)
-
-
-class InterpolateEngine:
-    """An engine which allows redirection of scipy.inteprolate to another lib at runtime."""
-
-    def __init__(self, interpolate=interpolate):
-        """Create a new interpolation engine.
-
-        Parameters
-        ----------
-        interpolate : `module`
-            a python module, with the same API as scipy.interpolate
-
-        """
-        self.interpolate = interpolate
-
-    def __getattr__(self, key):
-        """Get attribute.
-
-        Parameters
-        ----------
-        key : `str`
-            attribute name
-
-        """
-        return getattr(self.interpolate, key)
-
-
-class SpecialEngine:
-    """An engine which allows redirection of scipy.special to another lib at runtime."""
-    def __init__(self, special=special):
-        """Create a new special engine.
-
-        Parameters
-        ----------
-        special : `module`
-            a python module, with the same API as scipy.special
-
-        """
-        self.special = special
-
-    def __getattr__(self, key):
-        """Get attribute.
-
-        Parameters
-        ----------
-        key : `str`
-            attribute name
-
-        """
-        return getattr(self.special, key)
-
-
-class FFTEngine:
-    """An engine which allows redirecting of scipy.fft to another lib at runtime."""
-    def __init__(self, fft=fft):
-        """Create a new fft engine.
-
-        Parameters
-        ----------
-        fft : `module`
-            a python module, with the same API as scipy.fft
-
-        """
-        self.fft = fft
-
-    def __getattr__(self, key):
-        """Get attribute.
-
-        Parameters
-        ----------
-        key : `str`
-            attribute name
-
-        """
-        return getattr(self.fft, key)
-
-
-class NumpyEngine:
-    """An engine allowing an interchangeable backend for mathematical functions."""
-    def __init__(self, np=np):
-        """Create a new math engine.
-
-        Parameters
-        ----------
-        source : `module`
-            a python module.
-
-        """
-        self.numpy = np
-
-    def __getattr__(self, key):
-        """Get attribute.
-
-        Parameters
-        ----------
-        key : `str`
-            attribute name
-
-        """
-        return getattr(self.numpy, key)
-
-    def change_backend(self, backend):
-        """Run when changing the backend."""
-        self.source = backend
-
-
-np = NumpyEngine()
-special = SpecialEngine()
-ndimage = NDImageEngine()
-fft = FFTEngine()
-interpolate = InterpolateEngine()
+np = BackendShim(np)
+ndimage = BackendShim(ndimage)
+special = BackendShim(special)
+fft = BackendShim(fft)
+interpolate = BackendShim(interpolate)
 
 
 def jinc(r):
