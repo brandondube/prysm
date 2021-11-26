dd98adbcab6aef3cfb550adfe9a49ff1423bed8e
Brandon <brandondube@gmail.com>
2021-11-26 08:58:47 -0800

    tests/segmented: fix error in order of axes for basis expansion

--- tests/test_segmented.py
+++ tests/test_segmented.py
@@ -11,5 +11,5 @@ def test_segmented_hex_functions():
     csa = segmented.CompositeHexagonalAperture(x, y, 2, 0.2, .007, exclude=(0,))
     nms = [polynomials.noll_to_nm(j) for j in [1, 2, 3]]
     csa.prepare_opd_bases(polynomials.zernike_nm_sequence, nms)
-    csa.compose_opd(np.random.rand(3, len(csa.segment_ids)))
+    csa.compose_opd(np.random.rand(len(csa.segment_ids), len(nms)))
     assert csa
