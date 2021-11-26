40d88dfd6c7b2bcc545d5ce8ba27990afd009270
Brandon <brandondube@gmail.com>
2021-11-26 08:55:46 -0800

    segmented: + modal OPD synthesis (per-seg)

--- prysm/segmented.py
+++ prysm/segmented.py
@@ -1,10 +1,16 @@
 """Tools for working with segmented systems."""
+import inspect
 from collections import namedtuple
 
 import numpy as truenp
 
-from .geometry import regular_polygon
 from .mathops import np
+from .geometry import regular_polygon
+from .coordinates import cart_to_polar
+from .polynomials import sum_of_2d_modes
+
+FLAT_TO_FLAT_TO_VERTEX_TO_VERTEX = 2 / truenp.sqrt(3)
+
 
 Hex = namedtuple('Hex', ['q', 'r', 's'])
 
@@ -124,19 +130,19 @@ class CompositeHexagonalAperture:
 
         Parameters
         ----------
-        x : `numpy.ndarray`
+        x : numpy.ndarray
             array of x sample positions, of shape (m, n)
-        y : `numpy.ndarray`
+        y : numpy.ndarray
             array of y sample positions, of shape (m, n)
-        rings : `int`
+        rings : int
             number of rings in the structure
-        segment_diameter : `float`
+        segment_diameter : float
             flat-to-flat diameter of each segment, same units as x
-        segment_separation : `float`
+        segment_separation : float
             edge-to-nearest-edge distance between segments, same units as x
-        segment_angle : `float`, optional, {0, 90}
+        segment_angle : float, optional, {0, 90}
             rotation angle of each segment
-        exclude : sequence of `int`
+        exclude : sequence of int
             which segment numbers to exclude.
             defaults to all segments included.
             The 0th segment is the center of the array.
@@ -148,20 +154,148 @@ class CompositeHexagonalAperture:
             self.all_centers,
             self.windows,
             self.local_coords,
-            self. local_masks,
+            self.local_masks,
             self.segment_ids,
             self.amp
          ) = _composite_hexagonal_aperture(rings, segment_diameter, segment_separation,
                                            x, y, segment_angle, exclude)
+
+        self.x = x
+        self.y = y
+        self.segmentd_diameter = segment_diameter
+        self.segment_separation = segment_separation
+        self.segment_angle = segment_angle
         self.exclude = exclude
+
+    def prepare_opd_bases(self, basis_func, orders, basis_func_kwargs=None, normalization_radius=None):
+        """Prepare the polynomial bases for per-segment phase errors.
+
+        Parameters
+        ----------
+        basis_func : callable
+            a function with signature basis_func(orders, [x, y or r, t], **kwargs)
+            for example, zernike_nm_sequence from prysm.polyomials fits the bill
+        orders : iterable
+            sequence of polynomial orders or indices.
+            for example, zernike_nm_sequence may be combined with a monoindexing
+            function as e.g. orders=[noll_to_nm(j) for j in range(3,12)]
+        basis_func_kwargs : dict
+            any keyword arguments to pass to basis_func.  The spatial coordinates
+            will already be passed based on inspection of the function signature
+            and should not be attempted to be included here
+        normalization_radius : float
+            the normaliation radius to use to convert local surface coordinates
+            to normalized coordinates for an orthogonal polynomial.
+            if None, defaults to the half segment vertex to vertex distance,
+            v to v is 2/sqrt(3) times the segment diameter given in the constructor
+            if basis_func does not take arguments (r, t), the radius is assumed
+            to be equal in X and Y
+
+        """
+        # if the norm radius isn't given, assume it's V to V
+        # the conversion to a length two tuple is so that we know it's length
+        # two for either the r,t or x,y branch below
+        if normalization_radius is None:
+            normalization_radius = self.vtov/2
+
+        if not isinstance(normalization_radius, (tuple, list)):
+            normalization_radius = (normalization_radius, normalization_radius)
+
+        if basis_func_kwargs is None:
+            basis_func_kwargs = {}
+
+        # first thing to do, does basis_func take radial kwargs?
+        sig = inspect.signature(basis_func)
+        params = sig.parameters
+        gridcache = {}
+        polycache = {}
+        grids = []
+        bases = []
+        if 'r' in params and 't' in params:
+            nr = normalization_radius[0]
+            # there is high duplicity in most cases, e.g.
+            # a JWST-esque aperture painted on a 6.5m diameter
+            # array has 5 unique local grids, which with 18 segments
+            # means removing over 66% of the work and memory usage
+            # here we have three collections,
+            # gridcache is the unique grids
+            # grids is the grid for each segment, which may contain many
+            # duplicate elements but simplifies access for downstream routines,
+            # and polynomials which is the polynomial base for each segment,
+            # with the same duplicate note as the grids
+            for x, y in self.local_coords:
+                corner = float(x[0, 0])  # for Cupy support
+                key = (corner, *x.shape)
+                if key not in gridcache:
+                    r, t = cart_to_polar(x, y)
+                    r /= nr
+                    basis = list(basis_func(orders, r=r, t=t, **basis_func_kwargs))
+                    basis = np.asarray(basis)
+                    gridcache[key] = r, t
+                    polycache[key] = basis
+                else:
+                    r, t = gridcache[key]
+                    basis = polycache[key]
+
+                grids.append((r, t))
+                bases.append(basis)
+        else:
+            # assume x, y are the kwargs
+            for x, y in self.local_coords:
+                corner = float(x[0, 0])  # for Cupy support
+                key = (corner, *x.shape)
+                if key not in gridcache:
+                    xx = x / normalization_radius[0]
+                    yy = y / normalization_radius[1]
+                    basis = list(basis_func(orders, x=xx, y=yy, **basis_func_kwargs))
+                    basis = np.asarray(basis)
+                    gridcache[key] = xx, yy
+                    polycache[key] = basis
+                else:
+                    xx, yy = gridcache[key]
+                    basis = polycache[key]
+
+                grids.append((xx, yy))
+                bases.append(basis)
+
+        self.opd_bases = bases
+        self.opd_grids = grids
+        return grids, bases
+
+    def compose_opd(self, coefs, out=None):
+        """Compose per-segment optical path errors using the basis from prepare_opd_bases
+
+        Parameters
+        ----------
+        coefs : iterable
+            an iterable of coefficients for each segment present, i.e. excluding
+            those in the exclude list from the constructor
+            if an array, must be of shape (len(self.segment_ids), len(orders))
+            where orders comes from the proceeding call to prepare_opd_bases
+        out : numpy.ndarray
+            array to insert OPD into, allocated if None
+
+        Returns
+        -------
+        numpy.ndarray
+            OPD map of real datatype
+
+        """
+        if out is None:
+            out = np.zeros_like(self.x)
+        for win, mask, base, c in zip(self.windows, self.local_masks, self.opd_bases, coefs):
+            tile = sum_of_2d_modes(base, c)
+            tile *= mask
+            out[win] += tile
+
+        return out
 
 
 def _composite_hexagonal_aperture(rings, segment_diameter, segment_separation, x, y, segment_angle=90, exclude=(0,)):
     if segment_angle not in {0, 90}:
         raise ValueError('can only synthesize composite apertures with hexagons along a cartesian axis')
 
-    flat_to_flat_to_vertex_vertex = 2 / truenp.sqrt(3)
-    segment_vtov = segment_diameter * flat_to_flat_to_vertex_vertex
+    segment_vtov = segment_diameter * FLAT_TO_FLAT_TO_VERTEX_TO_VERTEX
     rseg = segment_vtov / 2
 
     # center segment
@@ -182,19 +316,23 @@ def _composite_hexagonal_aperture(rings, segment_diameter, segment_separation, x
 
     mask = np.zeros(x.shape, dtype=np.bool)
 
-    all_centers = [(0, 0)]
     segment_id = 0
-    segment_ids = [segment_id]
-    windows = [center_segment_window]
     xx = x[center_segment_window]
     yy = y[center_segment_window]
-    local_coords = [
-        (xx, yy)
-    ]
     center_mask = regular_polygon(6, rseg, xx, yy, center=(0, 0), rotation=segment_angle)
     if 0 not in exclude:
         mask[center_segment_window] |= center_mask
-    local_masks = [center_mask]
+        local_masks = [center_mask]
+        segment_ids = [0]
+        all_centers = [(0., 0.)]
+        windows = [center_segment_window]
+        local_coords = [(xx, yy)]
+    else:
+        local_masks = []
+        local_coords = []
+        segment_ids = []
+        all_centers = []
+        windows = []
     for i in range(1, rings+1):
         hexes = hex_ring(i)
         centers = [hex_to_xy(h, rseg+(segment_separation/2), rot=segment_angle) for h in hexes]
--- tests/test_segmented.py
+++ tests/test_segmented.py
@@ -1,10 +1,15 @@
 """Tests for special segmented system handling."""
 import pytest
 
-from prysm import coordinates, segmented
+import numpy as np
+
+from prysm import coordinates, segmented, polynomials
 
 
 def test_segmented_hex_functions():
     x, y = coordinates.make_xy_grid(256, diameter=2)
     csa = segmented.CompositeHexagonalAperture(x, y, 2, 0.2, .007, exclude=(0,))
+    nms = [polynomials.noll_to_nm(j) for j in [1, 2, 3]]
+    csa.prepare_opd_bases(polynomials.zernike_nm_sequence, nms)
+    csa.compose_opd(np.random.rand(3, len(csa.segment_ids)))
     assert csa
