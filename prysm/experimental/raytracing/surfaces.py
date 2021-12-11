7d580d5d7dd2a4dc7ccc84b54df89f5878de0d6e
Brandon <brandondube@gmail.com>
2021-12-11 11:48:09 -0800

    x/raytracing: rework Q2D and der with off-axis conic
    still not quite working

--- prysm/experimental/raytracing/surfaces.py
+++ prysm/experimental/raytracing/surfaces.py
@@ -206,6 +206,246 @@ def conic_sag_der(c, kappa, rho, phi=None):
     return (c * rho) / phi
 
 
+def off_axis_conic_sag(c, kappa, r, t, dx, dy=0):
+    """Sag of an off-axis conicoid
+
+    Parameters
+    ----------
+    c : float
+        axial curvature of the conic
+    kappa : float
+        conic constant
+    r : numpy.ndarray
+        radial coordinate, where r=0 is centered on the off-axis section
+    t : numpy.ndarray
+        azimuthal coordinate
+    dx : float
+        shift of the surface in x with respect to the base conic vertex,
+        mutually exclusive to dy (only one may be nonzero)
+        use dx=0 when dy != 0
+    dy : float
+        shift of the surface in y with respect to the base conic vertex
+
+    Returns
+    -------
+    numpy.ndarray
+        surface sag, z(x,y)
+
+    """
+    if dy != 0 and dx != 0:
+        raise ValueError('only one of dx/dy may be nonzero')
+
+    if dx != 0:
+        s = dx
+        oblique_term = 2 * s * r * np.cos(t)
+    else:
+        s = dy
+        oblique_term = 2 * s * r * np.sin(t)
+
+    aggregate_term = r * r + oblique_term + s * s
+    num = c * aggregate_term
+    csq = c * c
+    # typo in paper; 1+k => 1-k
+    den = 1 + np.sqrt(1 - (1 - kappa) * csq * aggregate_term)
+    return num / den
+
+
+def off_axis_conic_der(c, kappa, r, t, dx, dy=0):
+    """Radial and azimuthal derivatives of an off-axis conic.
+
+    Parameters
+    ----------
+    c : float
+        axial curvature of the conic
+    kappa : float
+        conic constant
+    r : numpy.ndarray
+        radial coordinate, where r=0 is centered on the off-axis section
+    t : numpy.ndarray
+        azimuthal coordinate
+    dx : float
+        shift of the surface in x with respect to the base conic vertex,
+        mutually exclusive to dy (only one may be nonzero)
+        use dx=0 when dy != 0
+    dy : float
+        shift of the surface in y with respect to the base conic vertex
+
+    Returns
+    -------
+    numpy.ndarray, numpy.ndarray
+        d/dr(z), d/dt(z)
+
+    """
+    if dy != 0 and dx != 0:
+        raise ValueError('only one of dx/dy may be nonzero')
+
+    cost = np.cos(t)
+    sint = np.sin(t)
+    if dx != 0:
+        s = dx
+        oblique_term = 2 * s * r * cost
+        ddr_oblique = 2 * r + 2 * s * cost
+        # I accept the evil in writing this the way I have
+        # to deduplicate the computation
+        ddt_oblique_ = r*(-s)*sint
+        ddt_oblique = 2 * ddt_oblique_
+    else:
+        s = dy
+        oblique_term = 2 * s * r * sint
+        ddr_oblique = 2 * r + 2 * s * sint
+        ddt_oblique_ = r*s*cost
+        ddt_oblique = 2 * ddt_oblique_
+
+    aggregate_term = r * r + oblique_term + s * s
+    csq = c * c
+    c3 = csq * c
+    # d/dr first
+    num = c * ddr_oblique
+    phi_kernel = (1 - kappa) * csq * aggregate_term
+    phi = np.sqrt(1 - phi_kernel)
+    phip1 = 1 + phi
+    phip1sq = phip1 * phip1
+    den = phip1
+    term1 = num / den
+
+    num = c3 * (1-kappa)*ddr_oblique * aggregate_term
+    den = (2 * phi) * phip1sq
+    term2 = num / den
+    dr = term1 + term2
+
+    # d/dt
+    num = c * ddt_oblique
+    den = phip1
+    term1 = num / den
+
+    num = c3 * (1-kappa) * ddt_oblique_ * aggregate_term
+    den = phi * phip1sq
+    term2 = num / den
+    dt = term1 + term2
+
+    return dr, dt
+
+
+def off_axis_conic_sigma(c, kappa, r, t, dx, dy=0):
+    """sigma (direction cosine projection term) for an off-axis conic.
+
+    See Eq. (5.2) of oe-20-3-2483.
+
+    Parameters
+    ----------
+    c : float
+        axial curvature of the conic
+    kappa : float
+        conic constant
+    r : numpy.ndarray
+        radial coordinate, where r=0 is centered on the off-axis section
+    t : numpy.ndarray
+        azimuthal coordinate
+    dx : float
+        shift of the surface in x with respect to the base conic vertex,
+        mutually exclusive to dy (only one may be nonzero)
+        use dx=0 when dy != 0
+    dy : float
+        shift of the surface in y with respect to the base conic vertex
+
+    Returns
+    -------
+    sigma(r,t)
+
+    """
+    if dy != 0 and dx != 0:
+        raise ValueError('only one of dx/dy may be nonzero')
+
+    if dx != 0:
+        s = dx
+        oblique_term = 2 * s * r * np.cos(t)
+    else:
+        s = dy
+        oblique_term = 2 * s * r * np.sin(t)
+
+    aggregate_term = r * r + oblique_term + s * s
+    csq = c * c
+    num = np.sqrt(1 - (1-kappa) * csq * aggregate_term)
+    den = np.sqrt(1 + kappa * csq * aggregate_term)  # flipped sign, 1-kappa
+    return num / den
+
+
+def off_axis_conic_sigma_der(c, kappa, r, t, dx, dy=0):
+    """Lowercase sigma (direction cosine projection term) for an off-axis conic.
+
+    See Eq. (5.2) of oe-20-3-2483.
+
+    Parameters
+    ----------
+    c : float
+        axial curvature of the conic
+    kappa : float
+        conic constant
+    r : numpy.ndarray
+        radial coordinate, where r=0 is centered on the off-axis section
+    t : numpy.ndarray
+        azimuthal coordinate
+    dx : float
+        shift of the surface in x with respect to the base conic vertex,
+        mutually exclusive to dy (only one may be nonzero)
+        use dx=0 when dy != 0
+    dy : float
+        shift of the surface in y with respect to the base conic vertex
+
+    Returns
+    -------
+    numpy.ndarray, numpy.ndarray
+        d/dr(z), d/dt(z)
+
+    """
+    if dy != 0 and dx != 0:
+        raise ValueError('only one of dx/dy may be nonzero')
+
+    cost = np.cos(t)
+    sint = np.sin(t)
+    if dx != 0:
+        s = dx
+        oblique_term = 2 * s * r * cost
+        ddr_oblique = 2 * r + 2 * s * cost
+        # I accept the evil in writing this the way I have
+        # to deduplicate the computation
+        ddt_oblique_ = r*(-s)*sint
+        ddt_oblique = 2 * ddt_oblique_
+    else:
+        s = dy
+        oblique_term = 2 * s * r * sint
+        ddr_oblique = 2 * r + 2 * s * sint
+        ddt_oblique_ = r*s*cost
+        ddt_oblique = 2 * ddt_oblique_
+
+    aggregate_term = r * r + oblique_term + s * s
+    csq = c * c
+    # d/dr first
+    phi_kernel = (1 - kappa) * csq * aggregate_term
+    phi = np.sqrt(1 - phi_kernel)
+    notquitephi = np.sqrt(1 + kappa * csq * aggregate_term)
+    num = csq * ddr_oblique * phi
+    den = 2 * (1 - csq * kappa * aggregate_term) ** (3/2)
+    term1 = num / den
+
+    num = csq * (1 - kappa) * ddr_oblique
+    den = 2 * phi * notquitephi  # slight difference in writing (2*phi*phi)
+    term2 = num / den
+    dr = term1 - term2
+
+    # d/dt
+    num = csq * (1 - kappa) * ddt_oblique_
+    den = phi * notquitephi
+    term1 = num/den
+
+    num = csq * kappa * ddt_oblique_ * phi
+    den = (csq * kappa * aggregate_term + 1) ** (3/2)
+    term2 = num / den
+    dt = term1 + term2  # minus in writing, but sine/cosine
+    # dt *= kappa
+    return dr, dt
+
+
 def Q2d_and_der(cm0, ams, bms, x, y, normalization_radius, c, k, dx=0, dy=0):
     """Q-type freeform surface, with base (perhaps shifted) conicoic.
 
@@ -251,35 +491,37 @@ def Q2d_and_der(cm0, ams, bms, x, y, normalization_radius, c, k, dx=0, dy=0):
     """
     # Q portion
     r, t = cart_to_polar(x, y)
-    r /= normalization_radius
-    z, zprimer, zprimet = compute_z_zprime_Q2d(cm0, ams, bms, r, t)
+    r2 = r / normalization_radius
+    # content of curly braces in B.1 from oe-20-3-2483
+    z, zprimer, zprimet = compute_z_zprime_Q2d(cm0, ams, bms, r2, t)
 
-    if dx != 0:
-        x = x + dx
-    if dy != 0:
-        y = y + dy
+    base_sag = off_axis_conic_sag(c, k, r, t, dx, dy)
+    base_primer, base_primet = off_axis_conic_der(c, k, r, t, dx, dy)
 
-    # no matter what need to do this again because of normalization radius
-    r, t = cart_to_polar(x, y)
-
-    rsq = r * r
-    phi = phi_spheroid(c, k, rsq)
-    base_sag = conic_sag(c, k, rsq, phi=phi)
-    base_sag_der = conic_sag_der(c, k, r, phi=phi)
-
-    q_prefix = 1 / phi
-    q_prefix_der = der_direction_cosine_spheroid(c, k, r, rhosq=rsq, phi=phi)
+    # Eq. 5.1/5.2
+    sigma = off_axis_conic_sigma(c, k, r, t, dx, dy)
+    sigmaprimer, sigmaprimet = off_axis_conic_sigma_der(c, k, r, t, dx, dy)
 
     # u = 1/phi
     # du = d/dr(1/phi)
     # v = q
     # dv = (q der)
 
+    # print('zt')
+    # print(zprimet)
     zprimer /= normalization_radius
-    zprimer2 = product_rule(q_prefix, z, q_prefix_der, zprimer)
+    zprimer2 = product_rule(sigma, z, sigmaprimer, zprimer)
+    # zprimet2 = zprimet
+    zprimet2 = product_rule(sigma, z, sigmaprimet, zprimet)
+    # print('zt2')
+    # print(zprimet2)
+    # zprimet2 = zprimet
     # don't need to adjust azimuthal derivative
-    z *= q_prefix
-    zprimet *= q_prefix
+    z *= sigma
+    # zprimet *= sigma
     z += base_sag
-    zprimer2 += base_sag_der
-    return z, zprimer2, zprimet
+    zprimer2 += base_primer
+    zprimet2 += base_primet
+    # print('zt2+bt')
+    # print(zprimet2)
+    return z, zprimer2, zprimet2
