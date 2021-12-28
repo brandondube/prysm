59ad723c1fca4b57a9cf3f7681e814bdf56e412e
Brandon <brandondube@gmail.com>
2021-12-28 11:17:12 -0500

    x/raytracing: add support for plotting lenses "properly"

--- prysm/experimental/raytracing/plotting.py
+++ prysm/experimental/raytracing/plotting.py
@@ -2,7 +2,7 @@
 
 from prysm.plotting import share_fig_ax
 
-from .surfaces import STYPE_REFLECT
+from .surfaces import STYPE_REFLECT, STYPE_REFRACT
 
 import numpy as np  # always numpy, matplotlib only understands numpy
 
@@ -59,6 +59,43 @@ def plot_rays(phist, lw=1, c='r', alpha=1, zorder=3, x='z', y='y', fig=None, ax=
     return fig, ax
 
 
+def _gather_inputs_for_surface_sag(surf, phist, j, points, y):
+    if surf.bounding is None:
+        # need to look at the raytrace to see bounding limits
+        p = phist[j+1]  # j+1, first element of phist is the start of the raytrace
+        xx = p[..., 0]
+        yy = p[..., 1]
+        mask = []
+        if y == 'y':
+            ymin = yy.min()
+            ymax = yy.max()
+            ypt = np.linspace(ymin, ymax, points)
+            ploty = ypt
+            xpt = 0
+        else:
+            xmin = xx.min()
+            xmax = xx.max()
+            xpt = np.linspace(xmin, xmax, points)
+            ploty = xpt
+            ypt = 0
+    else:
+        bound = surf.bounding
+        mx = bound['outer_radius']
+        r = np.linspace(-mx, mx, points)
+        mn = bound.get('inner_radius', 0)
+        ar = abs(r)
+        mask = ar < mn
+        ploty = r
+        if y == 'y':
+            ypt = r
+            xpt = 0
+        else:
+            xpt = r
+            ypt = 0
+
+    return xpt, ypt, mask, ploty
+
+
 def plot_optics(prescription, phist, mirror_backing=None, points=100,
                 lw=1, c='k', alpha=1, zorder=4,
                 x='z', y='y', fig=None, ax=None):
@@ -113,46 +150,42 @@ def plot_optics(prescription, phist, mirror_backing=None, points=100,
         if j == jj:
             break
         surf = prescription[j]
-        z = surf.P[2]
         if surf.typ == STYPE_REFLECT:
-            if surf.bounding is None:
-                # need to look at the raytrace to see bounding limits
-                p = phist[j+1]  # j+1, first element of phist is the start of the raytrace
-                xx = p[..., 0]
-                yy = p[..., 1]
-                mask = []
-                if y == 'y':
-                    ymin = yy.min()
-                    ymax = yy.max()
-                    ypt = np.linspace(ymin, ymax, points)
-                    ploty = ypt
-                    xpt = 0
-                else:
-                    xmin = xx.min()
-                    xmax = xx.max()
-                    xpt = np.linspace(xmin, xmax, points)
-                    ploty = xpt
-                    ypt = 0
-            else:
-                bound = surf.bounding
-                mx = bound['outer_radius']
-                r = np.linspace(-mx, mx, points)
-                mn = bound.get('inner_radius', 0)
-                ar = abs(r)
-                mask = ar < mn
-                ploty = r
-                if y == 'y':
-                    ypt = r
-                    xpt = 0
-                else:
-                    xpt = r
-                    ypt = 0
-
+            z = surf.P[2]
+            xpt, ypt, mask, ploty = _gather_inputs_for_surface_sag(surf, phist, j, points, y)
             sag = surf.F(xpt, ypt)
             sag += z
             sag[mask] = np.nan
             # TODO: mirror backing
             ax.plot(sag, ploty, c=c, lw=lw, alpha=alpha, zorder=zorder)
+        elif surf.typ == STYPE_REFRACT:
+            if (j + 1) == jj:
+                raise ValueError('cant draw a prescription that terminates on a refracting surface')
+
+            z = surf.P[2]
+            xpt, ypt, mask, ploty = _gather_inputs_for_surface_sag(surf, phist, j, points, y)
+            sag = surf.F(xpt, ypt)
+            sag += z
+            sag[mask] = np.nan
+
+            # now get the points for the second surface of the lens
+            j += 1
+            surf = prescription[j]
+            z = surf.P[2]
+            xpt2, ypt2, mask2, ploty2 = _gather_inputs_for_surface_sag(surf, phist, j, points, y)
+            sag2 = surf.F(xpt2, ypt2)
+            sag2 += z
+            sag2[mask2] = np.nan
+
+            # now bundle the two surfaces together so one line is drawn for the
+            # whole lens
+            first_x = sag[0]
+            first_y = ploty[0]
+            # the ::-1 are because we need to reverse the order of the second
+            # surface's points, so that matplotlib doesn't draw an X through the lens
+            xx = [*sag, *sag2[::-1], first_x]
+            yy = [*ploty, *ploty2[::-1], first_y]
+            ax.plot(xx, yy, c=c, lw=lw, alpha=alpha, zorder=zorder)
 
     return fig, ax
 
@@ -191,17 +224,59 @@ def plot_transverse_ray_aberration(phist, lw=1, c='r', alpha=1, zorder=3, axis='
     fig, ax = share_fig_ax(fig, ax)
 
     ph = np.asarray(phist)
-    xs = ph[..., 0]
-    ys = ph[..., 1]
-    zs = ph[..., 2]
     sieve = {
-        'x': xs,
-        'y': ys,
-        'z': zs,
+        'x': 0,
+        'y': 1,
     }
-    x = x.lower()
-    y = y.lower()
-    x = sieve[x]
-    y = sieve[y]
-    ax.plot(x, y, c=c, lw=lw, alpha=alpha, zorder=zorder)
+    axis = axis.lower()
+    axis = sieve[axis]
+    input_rays = ph[0, ..., axis]
+    output_rays = ph[-1, ..., axis]
+    ax.plot(input_rays, output_rays, c=c, lw=lw, alpha=alpha, zorder=zorder)
+    return fig, ax
+
+
+def plot_wave_aberration(phist, lw=1, c='r', alpha=1, zorder=3, axis='y', fig=None, ax=None):
+    """Plot the transverse ray aberration for a single ray fan.
+
+    Parameters
+    ----------
+    phist : list or numpy.ndarray
+        the first return from spencer_and_murty.raytrace,
+        iterable of arrays of length 3 (X,Y,Z)
+    lw : float, optional
+        linewidth
+    c : color
+        anything matplotlib interprets as a color, strings, 3-tuples, 4-tuples, ...
+    alpha : float
+        opacity of the rays, 1=fully opaque, 0=fully transparent
+    zorder : int
+        stack order in the plot, higher z orders are on top of lower z orders
+    axis : str, {'x', 'y'}
+        which ray position to plot, x or y
+    fig : matplotlib.figure.Figure
+        A figure object
+    ax : matplotlib.axes.Axis
+        An axis object
+
+    Returns
+    -------
+    matplotlib.figure.Figure
+        A figure object
+    matplotlib.axes.Axis
+        An axis object
+
+    """
+    fig, ax = share_fig_ax(fig, ax)
+
+    ph = np.asarray(phist)
+    sieve = {
+        'x': 0,
+        'y': 1,
+    }
+    axis = axis.lower()
+    axis = sieve[axis]
+    input_rays = ph[0, ..., axis]
+    output_rays = ph[-1, ..., axis]
+    ax.plot(input_rays, output_rays, c=c, lw=lw, alpha=alpha, zorder=zorder)
     return fig, ax
