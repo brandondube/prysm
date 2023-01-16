af4c0dcd0b26ec1265c24a7185124148c2afa3c0
Brandon Dube <brandon@retrorefractions.com>
2023-01-15 18:23:57 -0800

    x/PDI: *roomba noises*

--- prysm/experimental/pdi.py
+++ prysm/experimental/pdi.py
@@ -69,22 +69,18 @@ def rectangle_pulse(x, duty=0.5, amplitude=0.5, offset=0.5, period=2*np.pi):
     return y
 
 
-def sin_grating(x):
-    return np.sin
-
-
 class PSPDI:
     """Phase Shifting Point Diffraction Interferometer (Medecki Interferometer)."""
 
     def __init__(self, x, y, efl, epd, wavelength,
-                 test_arm_offset,
-                 test_arm_fov,
+                 test_arm_offset=64,
+                 test_arm_fov=64,
                  test_arm_samples=256,
                  test_arm_transmissivity=1,
                  pinhole_diameter=0.25,
                  pinhole_samples=128,
                  grating_rulings=64,
-                 grating_type='ronchi',
+                 grating_type='sin_amp',
                  grating_axis='x'):
         """Create a new PS/PDI or Medecki Interferometer.
 
@@ -103,7 +99,9 @@ class PSPDI:
         wavelength : float
             wavelength of light, um
         test_arm_offset : float
-            TODO
+            offset of the window for the test arm, in lambda/D
+            this number should only ever be different to grating_rulings
+            when you wish to model system misalignments
         test_arm_fov : float
             diameter of the circular hole placed at the m=+1 focus, units of
             lambda/D
@@ -177,7 +175,6 @@ class PSPDI:
         self.pinhole_fov_radius = pinhole_samples/2*self.dx_pinhole
 
         xph, yph = make_xy_grid(pinhole_samples, diameter=2*self.pinhole_fov_radius)
-        # self.dx_pinhole = xph[0, 1] - x[0, 0]
         rphsq = xph*xph + yph*yph
         self.pinhole = circle((pinhole_diameter/2)**2, rphsq)
 
@@ -204,8 +201,6 @@ class PSPDI:
 
         efl = self.efl
         if debug:
-            # shift2 = (-self.test_arm_shift[0], 0)
-            # shift2 = (0, 0)
             ref_beam, ref_at_fpm, ref_after_fpm = \
                 i.to_fpm_and_back(efl, self.pinhole, self.dx_pinhole, return_more=True)
             test_beam, test_at_fpm, test_after_fpm = \
@@ -233,193 +228,6 @@ class PSPDI:
                 }
             }
         return total_field.intensity
-
-class PSPDI2:
-    """Phase Shifting Point Diffraction Interferometer (Medecki Interferometer)."""
-
-    def __init__(self, x, y, efl, epd, wavelength,
-                 test_arm_offset,
-                 test_arm_fov,
-                 test_arm_samples=256,
-                 test_arm_transmissivity=1,
-                 pinhole_diameter=0.25,
-                 pinhole_samples=128,
-                 grating_rulings=64,
-                 grating_type='ronchi',
-                 grating_axis='x'):
-        """Create a new PS/PDI or Medecki Interferometer.
-
-        Parameters
-        ----------
-        x : numpy.ndarray
-            x coordinates for arrays that will be passed to forward_model
-            not normalized
-        y : numpy.ndarray
-            y coordinates for arrays that will be passed to forward_model
-            not normalized
-        efl : float
-            focal length in the focusing space behind the grating
-        epd : float
-            entrance pupil diameter, mm
-        wavelength : float
-            wavelength of light, um
-        test_arm_offset : float
-            TODO
-        test_arm_fov : float
-            diameter of the circular hole placed at the m=+1 focus, units of
-            lambda/D
-        test_arm_samples : int
-            samples to use across the clear window at the m=1 focus
-        test_arm_transmissivity : float
-            transmissivity (small r; amplitude) of the test arm, if the
-            substrate has an AR coating with reflectance R,
-            then this has value 1-sqrt(R) modulo absorbtion in the coating
-
-            The value of this parameter must be optimized to maximize fringe
-            visibility for peak performance
-        pinhole_diameter : float
-            diameter of the pinhole placed at the m=0 focus
-        pinhole_samples : int
-            number of samples across the pinhole placed at the m=0 focus
-        grating_rulings : float
-            number of rulings per EPD in the grating
-        grating_type : str, {'ronchi'}
-            type of grating used in the interferometer
-        grating_axis : str, {'x', 'y'}
-            which axis the orientation of the grating is in
-
-        """
-        grating_type = grating_type.lower()
-        grating_axis = grating_axis.lower()
-        # munge
-        if grating_type not in ('ronchi', 'sin_amp'):
-            raise ValueError('only ronchi gratings supported for now')
-        # inputs
-        self.x = x
-        self.y = y
-        self.dx = x[0, 1] - x[0, 0]
-        self.efl = efl
-        self.epd = epd
-        self.wavelength = wavelength
-        self.fno = efl/epd
-        self.flambd = self.fno * self.wavelength
-
-        # grating synthesis
-        self.grating_rulings = grating_rulings
-        self.grating_period = self.epd/grating_rulings
-        self.grating_type = grating_type
-        self.grating_axis = grating_axis
-
-        if grating_type == 'ronchi':
-            f = partial(rectangle_pulse, duty=0.5, amplitude=0.5, offset=0.5, period=self.grating_period)
-        elif grating_type == 'sin_amp':
-            def f(x):
-                prefix = grating_rulings*np.pi/(epd/2)
-                sin =np.sin(prefix*x)
-                return (sin+1)/2
-
-        self.grating_func = f
-
-        self.test_arm_offset = test_arm_offset
-        self.test_arm_fov = test_arm_fov
-        self.test_arm_transmissivity = test_arm_transmissivity
-
-        focal_dx = self.flambd/8
-        fx, fy = make_xy_grid(x.shape, dx=focal_dx)
-        fr = np.hypot(fx, fy)
-        self.pinhole = circle(pinhole_diameter*self.flambd/2, fr)
-        focal_dx = fx[0, 1]-fx[0, 0]
-        self.test_mask = offset_circle(test_arm_fov*self.flambd/2, fx, fy, (self.test_arm_offset*self.flambd, 0))
-        self.test_mask = self.test_mask * self.test_arm_transmissivity
-        self.total_mask = self.test_mask + self.pinhole
-
-    def forward_model(self, wave_in, phase_shift=0, debug=False):
-        # reference wave
-        if phase_shift != 0:
-            # user gives value in [0,2pi] which maps 2pi => period
-            phase_shift = phase_shift / (2*np.pi) * self.grating_period
-            x = self.x + phase_shift
-        else:
-            x = self.x
-        grating = self.grating_func(x)
-        i = wave_in * grating
-        if not isinstance(i, WF):
-            i = WF(i, self.wavelength, self.dx)
-
-        focal1 = i.focus(self.efl, Q=1)
-        focal2 = focal1 * self.total_mask
-        pip = focal2.unfocus(self.efl, Q=1).crop(self.x.shape[0]//6)
-        return pip.intensity
-
-
-def four_frame_psi(g0, g1, g2, g3):
-    """Sasaki algorithm.
-
-    Ref.
-    """
-    was_rd = isinstance(g0, RichData)
-    if was_rd:
-        g00 = g0
-        g0, g1, g2, g3 = g0.data, g1.data, g2.data, g3.data
-
-    # Sasaki from degroot
-    num = g0 + g1 - g2 - g3
-    den = g0 - g1 + g2 - g3
-
-    # other degroot
-    num = g3 - g1
-    den = g0 - g2
-
-    out = np.arctan2(num, den)
-    if was_rd:
-        out = RichData(out, g00.dx, g00.wavelength)
-
-    return out
-
-
-def five_frame_psi(g0, g1, g2, g3, g4):
-    """Schwider-Hariharan algorithm.
-
-    Ref.
-    Digital phase-shifting interferometry: a simple error-compensating phase calculation algorithm.
-    doi.org/10.1364/AO.26.002504
-
-    Expects phase shifts -180, -90, 0, 90, 180 deg
-    or -pi, -pi/2, 0, +pi/2, +pi
-
-    Parameters
-    ----------
-    g0 : numpy.ndarray
-        frame corresponding to -pi phase shift
-    g1 : numpy.ndarray
-        frame corresponding to -pi/2 phase shift
-    g2 : numpy.ndarray
-        frame corresponding to 0 phase shift
-    g3 : numpy.ndarray
-        frame corresponding to +pi/2 phase shift
-    g4 : numpy.ndarray
-        frame corresponding to +pi phase shift
-
-    Returns
-    -------
-    numpy.ndarray
-        wrapped phase estimate
-
-
-    """
-    was_rd = isinstance(g0, RichData)
-    if was_rd:
-        g00 = g0
-        g0, g1, g2, g3, g4 = g0.data, g1.data, g2.data, g3.data, g4.data
-
-    num = 2*(g1-g3)
-    den = -(g0+g4) + 2*g2
-    out = np.arctan2(num, den)
-    if was_rd:
-        out = RichData(out, g00.dx, g00.wavelength)
-
-    return out
-    # return np.arctan2(num, den)
 
 
 def degroot_formalism_psi(gs, ss, cs):
@@ -486,6 +294,7 @@ def degroot_formalism_psi(gs, ss, cs):
         out = RichData(out, g00.dx, g00.wavelength)
 
     return out
+
 
 def unwrap_phase(wrapped):
     was_rd = isinstance(wrapped, RichData)
