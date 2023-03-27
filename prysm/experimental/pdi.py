7b4d4081f2220c006db089572221669a73889c1b
Brandon Dube <brandon@retrorefractions.com>
2023-03-27 16:13:06 -0700

    x/pdi: refactor out PSI logic to its own file

--- prysm/experimental/pdi.py
+++ prysm/experimental/pdi.py
@@ -2,28 +2,10 @@
 
 from functools import partial
 
-import numpy as truenp
-
-from prysm._richdata import RichData
 from prysm.mathops import np
-from prysm.coordinates import make_xy_grid, cart_to_polar
+from prysm.coordinates import make_xy_grid
 from prysm.propagation import Wavefront as WF
-from prysm.geometry import circle, truecircle, offset_circle
-from prysm.fttools import fftrange, forward_ft_unit
-
-from skimage.restoration import unwrap_phase as ski_unwrap_phase
-
-
-FIVE_FRAME_PSI_NOMINAL_SHIFTS = (-np.pi, -np.pi/2, 0, +np.pi/2, +np.pi)
-FOUR_FRAME_PSI_NOMINAL_SHIFTS = (0, np.pi/2, np.pi, 3/2*np.pi)
-
-ZYGO_THIRTEEN_FRAME_SHIFTS = fftrange(13) * np.pi/4
-ZYGO_THIRTEEN_FRAME_SS = (-3, -4, 0, 12, 21, 16, 0, -16, -21, -12, 0, 4, 3)
-ZYGO_THIRTEEN_FRAME_CS = (0, -4, -12, -12, 0, 16, 24, 16, 0, -12, -12, -4, 0)
-
-SCHWIDER_SHIFTS = fftrange(5) * np.pi/4
-SCHWIDER_SS = (0, 2, 0, -2, 0)
-SCHWIDER_CS = (-1, 0, 2, 0, -1)
+from prysm.geometry import circle
 
 
 def rectangle_pulse(x, duty=0.5, amplitude=0.5, offset=0.5, period=2*np.pi):
@@ -149,8 +131,18 @@ class PSPDI:
         elif grating_type == 'sin_amp':
             def f(x):
                 prefix = grating_rulings*np.pi/(epd/2)
-                sin =np.sin(prefix*x)
-                return (sin+1)/2
+                sin = np.sin(prefix*x)
+
+                # this does not work the way you expect/want;
+                # can't improve efficiency by weakening a sine amp grating
+                # square wave with low duty cycle may be best, but brutal
+                # to model
+                # to make [0,1] => (sin+1)/2
+                # want to make [1-a,1], where a = amp
+                shifted_sin = (sin+1)/2
+                A = 0.1
+                squished = shifted_sin * A
+                return 1 - squished
         else:
             raise ValueError('unsupported grating type')
 
@@ -171,7 +163,8 @@ class PSPDI:
 
         self.pinhole_diameter = pinhole_diameter * self.flambd
         self.pinhole_samples = pinhole_samples
-        self.dx_pinhole = pinhole_diameter / (pinhole_samples-1) # -1 is an epsilon to make sure the circle is wholly inside the array
+        # -1 is an epsilon to make sure the circle is wholly inside the array
+        self.dx_pinhole = pinhole_diameter / (pinhole_samples-1)
         self.pinhole_fov_radius = pinhole_samples/2*self.dx_pinhole
 
         xph, yph = make_xy_grid(pinhole_samples, diameter=2*self.pinhole_fov_radius)
@@ -228,85 +221,6 @@ class PSPDI:
                 }
             }
         return total_field.intensity
-
-
-def degroot_formalism_psi(gs, ss, cs):
-    """Peter de Groot's formalism for Phase Shifting Interferometry algorithms.
-
-    Parameters
-    ----------
-    gs : iterable
-        sequence of images
-    ss : iterable
-        sequence of numerator weights
-    cs : iterable
-        sequence of denominator weights
-
-    Returns
-    -------
-    ndarray
-        wrapped phase estimate
-
-    Notes
-    -----
-    Ref
-    "Measurement of transparent plates with wavelength-tuned
-    phase-shifting interferometry"
-
-    Peter DeGroot, Appl. Opt,  39, 2658-2663 (2000)
-    https://doi.org/10.1364/AO.39.002658
-
-    num = \sum {s_m * g_m}
-    den = \sum {c_m * g_m}
-    theta = arctan(num/dem)
-
-    Common/Sample formalisms,
-    Schwider-Harihan five-frame algorithms, pi/4 steps
-    s = (0, 2, 0, -2, 0)
-    c = (-1, 0, 2, 0, -1)
-
-    Zygo 13-frame algorithm, pi/4 steps
-    s = (-3, -4, 0, 12, 21, 16, 0, -16, -21, -12, 0, 4, 3)
-    c = (0, -4, -12, -12, 0, 16, 24, 16, 0, -12, -12, -4, 0)
-
-    Zygo 15-frame algorithm, pi/2 steps
-    s = (-1, 0, 9, 0, -21, 0, 29, 0, -29, 0, 21, 0, -9, 0, 1)
-    c = (0, -4, 0, 15, 0, -26, 0, 30, 0, -26, 0, 15, 0, -4, 0)
-
-    """
-    was_rd = isinstance(gs[0], RichData)
-    if was_rd:
-        g00 = gs[0]
-        gs = [g.data for g in gs]
-
-    num = np.zeros_like(gs[0])
-    den = np.zeros_like(gs[0])
-    for gm, sm, cm in zip(gs, ss, cs):
-        # PSI algorithms tend to be sparse;
-        # optimize against zeros
-        if sm != 0:
-            num += sm * gm
-        if cm != 0:
-            den += cm * gm
-
-    out = np.arctan2(num, den)
-    if was_rd:
-        out = RichData(out, g00.dx, g00.wavelength)
-
-    return out
-
-
-def unwrap_phase(wrapped):
-    was_rd = isinstance(wrapped, RichData)
-    if was_rd:
-        w0 = wrapped
-        wrapped = wrapped.data
-
-    out = ski_unwrap_phase(wrapped)
-    if was_rd:
-        out = RichData(out, w0.dx, w0.wavelength)
-
-    return out
 
 
 def evaluate_test_ref_arm_matching(debug_dict):
