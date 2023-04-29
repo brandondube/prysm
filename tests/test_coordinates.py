c0e27fd06661553a70abeff7fe71210781154e2b
Brandon Dube <brandon@retrorefractions.com>
2023-04-29 12:13:54 -0700

    clean up tests for changed rotation setup in coordinates

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -77,8 +77,8 @@ def test_resample_2d_complex_does_not_distort(data_2d_complex):
 def test_make_rotation_matrix_matches_scipy():
     from scipy.spatial.transform import Rotation as R
 
-    angles = (0, 30, 0)
-    sp = R.from_euler('ZXZ', angles, degrees=True).as_matrix()
+    angles = (1, 2, 3)
+    sp = R.from_euler('ZYX', angles, degrees=True).as_matrix()
     pry = coordinates.make_rotation_matrix(angles)
     assert np.allclose(sp, pry)
 
@@ -86,7 +86,17 @@ def test_make_rotation_matrix_matches_scipy():
 def test_plane_warping_pipeline_functions(data_2d):
     x, y, z = data_2d
     x, y = np.meshgrid(x, y)
-    m = coordinates.make_rotation_matrix((0, 30, 0))
-    x2, y2 = coordinates.apply_rotation_matrix(m, x, y)
-    regular = coordinates.regularize([x, y], [x2, y2], z)
-    assert regular.any()
+    shape = x.shape
+    R = coordinates.make_rotation_matrix((1, 2, 3))
+    oy, ox = [(s-1)/2 for s in shape]
+    y, x = [np.arange(s) for s in shape]
+    y, x = np.meshgrid(y, x)
+    Tin = coordinates.make_homomorphic_translation_matrix(-ox, -oy)
+    Tout = coordinates.make_homomorphic_translation_matrix(ox, oy)
+    R = coordinates.promote_3d_transformation_to_homography(R)
+    Mfwd = Tout@(R@Tin)
+    Mfwd = coordinates.drop_z_3d_transformation(Mfwd)
+    Mifwd = np.linalg.inv(Mfwd)
+    xfwd, yfwd = coordinates.apply_homography(Mifwd, x, y)
+    zp = coordinates.warp(z, xfwd, yfwd)
+    assert zp.any()
