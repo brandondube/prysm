2e2920e73c419bf6d1845afeeb01363b9200bf4f
Brandon Dube <brandon@retrorefractions.com>
2023-07-06 10:49:23 -0700

    degredations: modernize smear and jitter

--- prysm/degredations.py
+++ prysm/degredations.py
@@ -1,10 +1,11 @@
 """Degredations in the image chain."""
 
 from .mathops import np
+from .conf import config
 from .coordinates import cart_to_polar, polar_to_cart
 
 
-def smear_ft(fx, fy, width, angle):
+def smear_ft(fx, fy, width, height):
     """Analytic Fourier Transform (OTF) of smear.
 
     Parameters
@@ -15,8 +16,8 @@ def smear_ft(fx, fy, width, angle):
         Y spatial frequencies, units of reciprocal width
     width : float
         width of the smear, units of length (e.g. um)
-    angle : float
-        angle w.r.t the X axis of the smear, degrees
+    height : float
+        height of the smear, units of length (e.g. um)
 
     Returns
     -------
@@ -24,13 +25,18 @@ def smear_ft(fx, fy, width, angle):
         transfer function of the smear
 
     """
-    # TODO: faster to do inline projection of fx, fy?
-    if angle != 0:
-        rho, phi = cart_to_polar(fx, fy)
-        phi += np.radians(angle)
-        x, y = polar_to_cart(rho, phi)
+    assert width != 0 or height != 0, 'one of width or height must be nonzero'
+    if width != 0:
+        out1 = np.sinc(fx * width).astype(config.precision)
+    else:
+        out1 = 1
 
-    return np.sinc(x * width)
+    if height != 0:
+        out2 = np.sinc(fy * height).astype(config.precision)
+    else:
+        out2 = 1
+
+    return out1*out2
 
 
 def jitter_ft(fr, scale):
@@ -39,9 +45,10 @@ def jitter_ft(fr, scale):
     Parameters
     ----------
     fr : numpy.ndarray
-        radial spatial frequency, units of reciprocal scale
+        radial spatial frequency, units of reciprocal length, e.g. cy/mm
     scale : float
-        scale of the jitter
+        scale of the jitter, in same units as "dx"
+        e.g., if fr has units cy/mm, then scale has units mm
 
     Returns
     -------
@@ -49,5 +56,6 @@ def jitter_ft(fr, scale):
         transfer function of the jitter
 
     """
-    kernel = np.pi * scale / 2 * fr
-    return np.exp(-2 * kernel**2)
+    core = (np.pi*scale*fr)
+    out = np.exp(-2 * (core*core))
+    return out
