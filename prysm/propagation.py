brandondube/prysm

commit 61f7ee4f10754d1cb8eae90a019c55256c19ffa9

propagation: optimize angular spectrum transfer function impl

prysm/propagation.py
@@ -350,10 +350,12 @@ def angular_spectrum_transfer_function(samples, wvl, dx, z):
 
     wvl = wvl / 1e3
     ky, kx = (fft.fftfreq(s, dx).astype(config.precision) for s in samples)
-    ky = np.broadcast_to(ky, samples).swapaxes(0, 1)
-    kx = np.broadcast_to(kx, samples)
+    kxx = kx * kx
+    kyy = ky * ky
+    kyy = np.broadcast_to(ky, samples).swapaxes(0, 1)
+    kxx = np.broadcast_to(kx, samples)
 
-    return np.exp(-1j * np.pi * wvl * z * (kx*kx + ky*ky))
+    return np.exp(-1j * np.pi * wvl * z * (kxx + kyy))
 
 
 class Wavefront:

commit a4cdbcb05070e4af35a0ddd9e458039829cb5aee

propagation: fix regression in angular spectrum transfer function impl
This fixes typos in the optimization introduced in
61f7ee4f10754d1cb8eae90a019c55256c19ffa9.

prysm/propagation.py
@@ -405,8 +405,8 @@ def angular_spectrum_transfer_function(samples, wvl, dx, z):
     ky, kx = (fft.fftfreq(s, dx).astype(config.precision) for s in samples)
     kxx = kx * kx
     kyy = ky * ky
-    kyy = np.broadcast_to(ky, samples).swapaxes(0, 1)
-    kxx = np.broadcast_to(kx, samples)
+    kyy = np.broadcast_to(kyy, samples).swapaxes(0, 1)
+    kxx = np.broadcast_to(kxx, samples)
 
     return np.exp(-1j * np.pi * wvl * z * (kxx + kyy))
 