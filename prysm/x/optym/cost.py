dc620e943b7d8416595921a9dc4316b91a19bfcc
Brandon Dube <brandon@retrorefractions.com>
2024-01-21 09:42:13 -0800

    x/optym: add negative loglikelihood cost

--- prysm/x/optym/cost.py
+++ prysm/x/optym/cost.py
@@ -111,3 +111,42 @@ def mean_square_error(M, D, mask=None):
         grad = 2 * alpha * diff
 
     return cost, grad
+
+
+def negative_loglikelihood(y, yhat, mask=None):
+    """Negative log likelihood.
+
+    Parameters
+    ----------
+    y : numpy.ndarray
+        predicted values; typically the output of a model
+    yhat : numpy.ndarray
+        truth or target values
+    mask : numpy.ndarray, optional
+        True where M should contribute to the cost, False where it should not
+
+    Returns
+    -------
+    float, numpy.ndarray
+        cost, dcost/dy
+
+    """
+    if mask is not None:
+        y = y[mask]
+        yhat = yhat[mask]
+
+    sub1 = 1-y
+    sub2 = 1-yhat
+    prefix = 1/y.size  #               1-yhat        1-y  # NOQA flake8 doesn't like comment starting with space
+    cost = -prefix * (yhat*np.log(y) + (sub2)*np.log(sub1)).sum()
+
+    #                   1-yhat  1-y
+    dcost = (-yhat/y) + (sub2)/(sub1)
+    dcost *= prefix
+
+    if mask is not None:
+        dcost2 = np.zeros(mask.shape, dtype=y.dtype)
+        dcost2[mask] = dcost
+        dcost = dcost2
+
+    return cost, dcost
