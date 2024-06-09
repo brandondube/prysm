69115393fb7da92ea3bcc620d8f0bd185f34460a
Brandon Dube <brandon@retrorefractions.com>
2024-06-09 09:00:06 -0700

    migrate project to pyproject.toml

--- prysm/__init__.py
+++ prysm/__init__.py
@@ -1,5 +1,6 @@
 """prysm, a python optics module."""
-from pkg_resources import get_distribution
 
 # revisit the decision to export anything at the top level or not
-__version__ = get_distribution('prysm').version
+# import importlib.metadata
+
+# __version__ = importlib.metadata.version('your-package')
