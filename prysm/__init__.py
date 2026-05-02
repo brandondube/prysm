62c3919ebd1d2c81c281ee3dc9df0efb5a2fa33a
Brandon Dube <brandon@retrorefractions.com>
2026-05-02 13:34:18 -0700

    remove stale comments in init

--- prysm/__init__.py
+++ prysm/__init__.py
@@ -1,6 +1,1 @@
 """prysm, a python optics module."""
-
-# revisit the decision to export anything at the top level or not
-# import importlib.metadata
-
-# __version__ = importlib.metadata.version('your-package')
